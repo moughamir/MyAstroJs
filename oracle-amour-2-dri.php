69d9200c34ea71266e0d7a5d251fd4322d52a380
GuillaumeD <guillaumed.kgcom@gmail.com>
2016-07-15 10:18:51 +0200

    campagne oracle-amour-2 final text on DRI-POSTMAIL

--- oracle-amour-2-dri.php
+++ oracle-amour-2-dri.php
@@ -48,7 +48,9 @@ include('include/process-dri-postmailing.php');
             <div class="TextContent">
                 <p class="DescText-Title-Legend">Lucas<br/>Expert voyant-tarologue</p>
                 <div class="DescText-Content">
-                    <p class="result-Tarot-text"><span><?= $prenom ?></span> : of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                    <p class="result-Tarot-text"><span><?= $prenom ?></span> : les cartes de l’Oracle de l’amour sont d’une profondeur et d’une puissance telles, que seule une étude et une interprétation détaillées permettent d’en capter les messages. </p>
+                    <p>Ces messages qui s’offrent à nous comme une aide précieuse, pour construire la vie sentimentale à laquelle vous pensez actuellement.</p>
+                    <p>Laissez-moi vous traduire les messages que les cartes de l’Oracle ont à vous transmettre.</p>
                 </div>
             </div>
                     
