79e1c8d5b8417aacc233b858ac83e51d5ebf1ec8
GuillaumeD <guillaumed.kgcom@gmail.com>
2016-07-18 16:53:38 +0200

    campagne cycle-lunaire-tel method changed

--- cycle-lunaire-tel.php
+++ cycle-lunaire-tel.php
@@ -21,7 +21,10 @@ $prenom = $_SESSION['firstname'];
                                                     </p>
                                                 <?php } else { ?>
                                                 <form method="post">
-                                                    <input type="hidden" name="method" value="telnum-save" />
+                                                    <!-- ########## identification formulaire ########## -->
+                                                    <input type="hidden" name="save_tel" value="1" />
+                                                    <input type="hidden" name="tel_needed" value="1" />
+                                                    <!-- ############################################### -->
                                                     <div class="FormField">
                                                         <input type="tel" id="tel" name="tel" placeholder="Mon N° de téléphone" class="FormField-Input" required />
                                                     </div>
