7c76e4f72a62b6c1f208a3bca1068506539967a8
Mohamed Moughamir <moughamir@gmail.com>
2016-12-12 19:05:24 +0000

    prevision home screen done

--- prevision-2017.php
+++ prevision-2017.php
@@ -11,12 +11,248 @@
     <meta name="keywords" content=""/>
     <link rel="shortcut icon" href="images_landing/prevision-2017/favicon.ico">
     <title>Prévisions année 2017</title>
-    <link rel="stylesheet" href="css/prevision-2017.css" >
+    <link rel="stylesheet" href="css/prevision-2017.css">
+    <link rel="shortcut icon" href="images_landing/previsions-2017/favicon.ico">
 </head>
 <body>
-<p class="SiteCopyright"><?php include('include/footer_copyright.php'); ?></p>
+<div class="PageWrapper">
+    <section class="site">
+        <header class="site-header"><a href="#"><img src="images_landing/site-logo.png" alt="MyAstro" class="site-logo"></a>
+        </header>
+        <main>
+            <ul class="zodiac grid">
+                <div class="grid-3">
+                    <div class="one-third">
+                        <li class="sign grid__item">
+                            <a href="#sign-1" class="sign-link">
+                                <div class="content aquarius">
+                                    <h2 class="sign-name">Verseau</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container">
+                                            <img src="images_landing/previsions-2017/astro/verseau.png"
+                                                 alt="Verseau" class="sign-1"></div>
+                                    </div>
+                                    <h3 class="sign-period">20 janvier > 19 février</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                    <div class="one-third"></div>
+                    <div class="one-third">
+                        <li class="sign grid__item">
+                            <a href="#sign-3" class="sign-link">
+                                <div class="content capricorn">
+                                    <h2 class="sign-name">Capricorne</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/capricorne.png"
+                                                    alt="Capricorne" class="sign-2"></div>
+                                    </div>
+                                    <h3 class="sign-period">22 décembre > 19 janvier</h3>
+                                </div>
+                            </a>
+                        </li>
+                    </div>
+                </div>
+                <div class="grid-3">
+                    <div class="one-third"></div>
+                    <div class="one-third">
+                        <li class="sign grid__item">
+                            <a href="#sign-2" class="sign-link">
+                                <div class="content pisces">
+                                    <h2 class="sign-name">Poisson</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/poisson.png"
+                                                    alt="Poisson" class="sign-3"></div>
+                                    </div>
+                                    <h3 class="sign-period">20 février > 20 mars</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                    <div class="one-third"></div>
+                </div>
+                <div class="grid-3">
+                    <div class="one-third">
+                        <li class="sign grid__item">
+                            <a href="#sign-4" class="sign-link">
+                                <div class="content taurus">
+                                    <h2 class="sign-name">Taureau</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/taureau.png"
+                                                    alt="Taureau" class="sign-4"></div>
+                                    </div>
+                                    <h3 class="sign-period">20 avril > 21 mai</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                    <div class="one-third"></div>
+                    <div class="one-third">
+                        <li class="sign grid__item">
+                            <a href="#sign-6" class="sign-link">
+                                <div class="content aries">
+                                    <h2 class="sign-name">Bélier</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/belier.png"
+                                                    alt="Bélier"
+                                                    class="sign-5"></div>
+                                    </div>
+                                    <h3 class="sign-period">21 mars > 19 avril</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                </div>
+                <div class="grid-3">
+                    <div class="one-third"></div>
+                    <div class="one-third">
+                        <li class="sign grid__item">
+                            <a href="#sign-5" class="sign-link">
+                                <div class="content gemini">
+                                    <h2 class="sign-name">Gémeaux</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/gemeaux.png"
+                                                    alt="Gémeaux" class="sign-6"></div>
+                                    </div>
+                                    <h3 class="sign-period">22 mai > 21 juin</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                    <div class="one-third"></div>
+                </div>
+                <div class="grid-3">
+                    <div class="one-third">
+                        <li class="sign grid__item"><a href="#sign-7" class="sign-link">
+                                <div class="content leo">
+                                    <h2 class="sign-name">Lion</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/lion.png"
+                                                    alt="Lion"
+                                                    class="sign-7"></div>
+                                    </div>
+                                    <h3 class="sign-period">22 juillet > 22 août</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                    <div class="one-third"></div>
+                    <div class="one-third">
+                        <li class="sign grid__item"><a href="#sign-9" class="sign-link">
+                                <div class="content cancer">
+                                    <h2 class="sign-name">Cancer</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/cancer.png"
+                                                    alt="Cancer"
+                                                    class="sign-8"></div>
+                                    </div>
+                                    <h3 class="sign-period">22 juin > 21 juillet</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                </div>
+                <div class="grid-3">
+                    <div class="one-third"></div>
+                    <div class="one-third">
+                        <li class="sign grid__item"><a href="#sign-8" class="sign-link">
+                                <div class="content virgo">
+                                    <h2 class="sign-name">Vierge</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/vierge.png"
+                                                    alt="Vierge"
+                                                    class="sign-9"></div>
+                                    </div>
+                                    <h3 class="sign-period">23 août > 22 septembre</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                    <div class="one-third"></div>
+                </div>
+                <div class="grid-3">
+                    <div class="one-third">
+                        <li class="sign grid__item"><a href="#sign-10" class="sign-link">
+                                <div class="content scorpio">
+                                    <h2 class="sign-name">Scorpion</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/scorpion.png"
+                                                    alt="Scorpion" class="sign-10"></div>
+                                    </div>
+                                    <h3 class="sign-period">23 octobre > 22 novembre</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                    <div class="one-third"></div>
+                    <div class="one-third">
+                        <li class="sign grid__item"><a href="#sign-12" class="sign-link">
+                                <div class="content libra">
+                                    <h2 class="sign-name">Balance</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/balance.png"
+                                                    alt="Balance" class="sign-11"></div>
+                                    </div>
+                                    <h3 class="sign-period">23 septembre > 22 octobre</h3>
+                                </div>
+                            </a></li>
+                    </div>
+                </div>
+                <div class="grid-3">
+                    <div class="one-third"></div>
+                    <div class="one-third">
+                        <li class="sign grid__item"><a href="#sign-11" class="sign-link">
+                                <div class="content sagittarius">
+                                    <h2 class="sign-name">Sagittaire</h2>
+                                    <div class="sign-image">
+                                        <div class="img-container"><img
+                                                    src="images_landing/previsions-2017/astro/sagittaire.png"
+                                                    alt="Sagittaire" class="sign-12"></div>
+                                    </div>
+                                </div>
+                                <h3 class="sign-period">23 novembre > 21 décembre</h3>
+                            </a></li>
+                    </div>
+                    <div class="one-third"></div>
+                </div>
+
+
+            </ul>
+        </main>
+    </section>
+    <footer class="SiteFooter">
+        <div class="FooterWrapper">
+            <ul class="ReassuranceList">
+                <li class="ReassuranceList-Item">
+                    <span class="ReassuranceList-Item-Img star"></span>
+                    <span class="ReassuranceList-Item-Txt">Voyant <strong>sérieux reconnus</strong> <br/> pour leur <strong>savoir faire</strong></span>
+                </li>
+                <li class="ReassuranceList-Item">
+                    <span class="ReassuranceList-Item-Img lock"></span>
+                    <span class="ReassuranceList-Item-Txt">Consultations 100% <br/><strong>discrètes & anonymes</strong></span>
+                </li>
+                <li class="ReassuranceList-Item">
+                    <span class="ReassuranceList-Item-Img chat"></span>
+                    <span class="ReassuranceList-Item-Txt">Interprétation<br/><strong>immédiate</strong></span>
+                </li>
+                <li class="ReassuranceList-Item">
+                    <span class="ReassuranceList-Item-Img trophy"></span>
+                    <span class="ReassuranceList-Item-Txt">My Astro leader <br/><strong>depuis 2007</strong></span>
+                </li>
+                <li class="ReassuranceList-Item">
+                    <span class="ReassuranceList-Item-Img card"></span>
+                    <span class="ReassuranceList-Item-Txt">Paiement <br/><strong>sécurisé par CB</strong></span>
+                </li>
+            </ul>
+        </div>
+    </footer>
+    <p class="SiteCopyright"><?php include('include/footer_copyright.php'); ?></p>
+</div>
 <!-- #### SCRIPTS #### -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
+<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
 <script src="js/formValidator.js"></script>
 <!-- #### REMARKETINGS #### -->
 <?php
@@ -24,6 +260,6 @@ include('include/remarketing/adwords.php');
 include('include/remarketing/analytics.php');
 include('include/remarketing/facebook.php');
 ?>
-<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Oswald:300,400" rel="stylesheet">
 </body>
 </html>
