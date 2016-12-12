9aea43b0e340fc0ebf9805158630d8866f7405af
Mohamed Moughamir <moughamir@gmail.com>
2016-12-12 11:02:53 +0000

    A]

--- prevision-2017.php
+++ prevision-2017.php
@@ -2,16 +2,16 @@
 
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
-  <meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="" />
-	<meta name="keywords" content="" />
-	<meta name="author" content="Codrops" />
-	<link rel="shortcut icon" href="images_landing/prevision-2017/favicon.ico">
-	<title>Prévisions année 2017</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+    <link rel="shortcut icon" href="images_landing/prevision-2017/favicon.ico">
+    <title>Prévisions année 2017</title>
+    <link rel="stylesheet" href="css/prevision-2017.css" >
 </head>
 <body>
 <p class="SiteCopyright"><?php include('include/footer_copyright.php'); ?></p>
@@ -24,6 +24,6 @@ include('include/remarketing/adwords.php');
 include('include/remarketing/analytics.php');
 include('include/remarketing/facebook.php');
 ?>
-<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet" type="text/css"/>
 </body>
 </html>
