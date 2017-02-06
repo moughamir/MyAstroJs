33c47e6cdd19c9b4dfcb8de6707d12e38336e27a
MOUGHAMIR Mohamed <moughamir@gmail.com>
2017-02-06 08:45:55 +0000

    Init name compatibility dri

---
+++ compatibilite-prenom.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Compatibilité</title>
+  <link rel="stylesheet" href="css/nameCompa.css" type="text/css" />
+</head>
+<body>
+  <main>
+    <header>
+      <h1>Testez la compatibilité de vos prénoms.</h1>
+    </header>
+    <section>
+      <article>
+        <h2>Vos prénoms sont-ils compatibles?</h2>
+        <p>Pour vous apporter des réponses claires. la compatibilité des prénoms vous dècrit en détail les possibilités amoureuse ou non avec cette autre personne.<br/>
+        évaluez les chances de réussite de cette relation.<br/>
+        Découvrez en un CLIC l’affinité AMOUREUSE de vos deux Prénoms.</p>
+      </article>
+      <form>
+        <label for="mon-prenom">Mon prénom</label>
+        <input class="name" type="text" name="mon-prenom" placeholder="Mon prénom" value="MEd"/>
+        <label for="mon-prenom">Son prénom</label>
+        <input class="name0" type="text" name="son-prenom" placeholder="Son prénom"/>
+        <input class="btn-submit" type="submit" value="Voir le Résultat"/>
+      </form>
+    </section>
+  </main>
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script type="text/javascript" src="js/nameCompa.js"></script>
+</body>
+</html>
---
+++ js/nameCompa.js
@@ -0,0 +1,103 @@
+/**
+ * Title:  Test Compatibilté des Prénom
+ * Author: Mohamed Moughamir <hello@omnizya.com>
+ * Copyright: KG-Com 2017
+*/
+/*
+global $
+*/
+
+const letters = {
+  A:1, J:1, S:1,
+  B:2, K:2, T:2,
+  C:3, L:3, U:3,
+  D:4, M:4, V:4,
+  E:5, N:5, W:5,
+  F:6, O:6, X:6,
+  G:7, P:7, Y:7,
+  H:8, Q:8, Z:8,
+  I:9, R:9
+},
+paths = {
+  'one' : 'Entre vous et {{OTHERPERSON}}, c’est toute feu toute flamme ! Votre couple est la somme de vos deux personnalités, fortes, indépendantes et capables de se réaliser soi-même. Pour que votre histoire fonctionne, vos deux caractères doivent se compléter et chacun doit fournir des efforts très particuliers. Votre relation avec {{OTHERPERSON}} est à prendre avec des pincettes, mais le jeu en vaut vraiment la chandelle. {{FIRSTPERSON}}, c’est en prenant du recul que vous avancerez efficacement…',
+  'two' : 'A première vue, vous n’êtes pas vraiment faits pour vivre l’un avec l’autre. Mais votre volonté, combinée à celle de {{OTHERPERSON}}, attenue cette fausse impression. Tout est possible avec l’état d’esprit approprié! {{FIRSTPERSON}}, allez-y pas à pas, en prenant soin de ne pas griller les étapes. Prenez le temps de réflexion nécessaire avant d’agir, sous peine de devoir faire face à bien des difficultés relationnelles.',
+  'three' : 'A première vue, vous n’êtes pas vraiment faits pour vivre l’un avec l’autre. Mais votre volonté, combinée à celle de {{OTHERPERSON}}, attenue cette fausse impression. Tout est possible avec l’état d’esprit approprié! {{FIRSTPERSON}}, allez-y pas à pas, en prenant soin de ne pas griller les étapes. Prenez le temps de réflexion nécessaire avant d’agir, sous peine de devoir faire face à bien des difficultés relationnelles.'
+  
+},
+user = '{{FIRSTPERSON}}',
+other = '{{OTHERPERSON}}',
+name = $('.name').val($(this).val().toUpperCase()),
+submit = $('.btn-submit');
+    
+    
+
+function simpleScore(name) {
+  var nameScore = 0;
+  for (var i = 0; i < name.length; i++ ) {
+    //name[i];
+    var curChar = name.charAt(i);
+    var curValue = letters[curChar];
+    
+  }
+}    
+    var nameScore = 0;
+    var singleDigitScore = nameScore;
+    
+    function scorifyName(name) {
+      
+      for( var i = 0; i < name.length; i++){
+        var curChar = name.charAt(i);
+        var curValue = letters[curChar]
+        
+        
+      }
+      nameScore = nameScore + curValue;
+      
+      while( singleDigitScore >= 10 ){
+        var total = 0;
+        var str = '' + singleDigitScore;
+        for( var i = 0; i < str.length; i++ ){
+          total = total + parseInt( str.charAt(i) );
+        }//for()
+        singleDigitScore = total;
+      }//while
+    }
+    
+    
+    //$('jqueryselector').val($(this).val().toUpperCase());
+    console.log( "Total score for this name is: " + scorifyName(name) );
+
+
+
+
+console.log("Single Digit score is: " + singleDigitScore);
+
+
+
+
+
+/**
+ * Find and Replace with regEx
+*/
+//function (search, replacement) {
+//    var target = this;
+//    return target.replace(new RegExp(search, 'g'), replacement);
+//};
+function findReplace(word) {
+  const regex = /word/g;
+  const str = `Entre vous et {{OTHERPERSON}}, c’est toute feu toute flamme ! Votre couple est la somme de vos deux personnalités, fortes, indépendantes et capables de se réaliser soi-même. Pour que votre histoire fonctionne, vos deux caractères doivent se compléter et chacun doit fournir des efforts très particuliers. Votre relation avec {{OTHERPERSON}} est à prendre avec des pincettes, mais le jeu en vaut vraiment la chandelle. {{FIRSTPERSON}}, c’est en prenant du recul que vous avancerez efficacement…`;
+  let m;
+  
+  while ((m = regex.exec(str)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+    
+    // The result can be accessed through the `m`-variable.
+    m.forEach((match, groupIndex) => {
+        console.log(`Found match, group ${groupIndex}: ${match}`);
+    });
+}
+}
+
