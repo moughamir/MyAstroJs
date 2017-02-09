597bcb153a1750ecf2b65a70381d613d226b2263
MOUGHAMIR Mohamed <moughamir@gmail.com>
2017-02-09 13:32:47 +0000

    Encapsulate the code and setting up placeholders changer

--- compatibilite-prenom.php
+++ compatibilite-prenom.php
@@ -1,32 +1,133 @@
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
-  <title>Compatibilité</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Compatibilité des Prénom</title>
+  <meta name="robots" content="noindex,nofollow" />
+  <link rel="icon" type="image/png" href="logo_myastro_32x32.jpg" />
+  <link href='//fonts.googleapis.com/css?family=Dancing+Script|Open+Sans' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
   <link rel="stylesheet" href="css/saint-valentin-2017.css" type="text/css" />
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
+
 <body>
-  <main>
-    <header>
-      <h1>Testez la compatibilité de vos prénoms.</h1>
-    </header>
-    <section>
-      <article>
-        <h2>Vos prénoms sont-ils compatibles?</h2>
-        <p>Pour vous apporter des réponses claires. la compatibilité des prénoms vous dècrit en détail les possibilités amoureuse ou non avec cette autre personne.<br/>
-        évaluez les chances de réussite de cette relation.<br/>
-        Découvrez en un CLIC l’affinité AMOUREUSE de vos deux Prénoms.</p>
-      </article>
-      <form>
-        <label for="mon-prenom">Mon prénom</label>
-        <input class="name" type="text" name="user" placeholder="Mon prénom"/>
-        <label for="mon-prenom">Son prénom</label>
-        <input class="name" type="text" name="other" placeholder="Son prénom"/>
-        <a class="btn-submit"/>Voir le Résultat</a>
-      </form>
-    </section>
-  </main>
-  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+  <header class="SiteHeader">
+    <div class="PageWrapper">
+      <span class="SiteLogo"></span>
+      <span class="PageLogo"><h1>Testez la compatibilité de vos prénoms.</h1></span>
+    </div>
+  </header>
+
+  <section class="ContentBand">
+
+    <div class="PageWrapper">
+      <div class="ContentBand-Column Form txtcenter" id="form-container">
+        <article class="gal-container">
+          <img src="images_landing/saint-valentin-2017/left.jpg" alt="" class="promo-img">
+          <img src="images_landing/saint-valentin-2017/right.jpg" alt="" class="promo-img">
+        </article>
+        <div id="photo" class="polaroid anim-photo1"></div>
+
+        <article class="FormContainer">
+
+          <div class="FormContainer overlay" id="form-overlay"></div>
+          <h2 class="FormContainer-Header">Vos prénoms sont-ils compatibles?</h2>
+          <p class="resault-text">Pour vous apporter des réponses claires. la compatibilité des prénoms vous dècrit en détail les possibilités amoureuse ou non avec cette autre personne.<br/> évaluez les chances de réussite de cette relation.<br/> Découvrez en un CLIC l’affinité
+            AMOUREUSE de vos deux Prénoms.</p><p class="more">Pour en savoir plus:</p>
+
+          <div class="FormContainer-Fields">
+
+            <p class="alert alert-danger" style="display: none"></p>
+
+            <form class="ajax">
+              <section class="step-1">
+                
+                <div class="FormField">
+                  <label for="mon-prenom" class="FormField-Label">Mon prénom</label>
+                  <input class="name FormField-Input" type="text" name="user" placeholder="Mon prénom" />
+                </div>
+                <div class="FormField">
+                  <label for="mon-prenom" class="FormField-Label">Son prénom</label>
+                  <input class="name FormField-Input" type="text" name="other" placeholder="Son prénom" />
+                </div>
+                
+                <a class="btn-submit" />Voir le Résultat</a>
+                
+              </section>
+              
+              <section class="resault hidden">
+                <div class="FormField">
+                  <label for="tel" class="FormField-Label">Mon numéro de téléphone</label>
+                  <input id="tel " type="tel" class="FormField-Input" name="tel" required/>
+                </div>
+                <div class="Fields-Table-Row">
+                  <label for="pays" class="FormField-Label">Votre pays</label>
+                  <div class="FormField">
+                    <select name="pays" id="pays" class="FormField-Input" required>
+                      <option value="" selected>Votre Pays</option>
+                      <option value="BE">Belgique</option>
+                      <option value="CA">Canada</option>
+                      <option value="LU">Luxembourg</option>
+                      <option value="CH">Suisse</option>
+                      <option value="FR" selected>France Métropolitaine</option>
+                      <optgroup label="DOM-TOM">
+                        <option value="MQ">Martinique</option>
+                        <option value="GP">Guadeloupe</option>
+                        <option value="GF">Guyane</option>
+                        <option value="RE">La Réunion</option>
+                        <option value="YT">Mayotte</option>
+                        <option value="PM">St Pierre et Miquelon</option>
+                        <option value="BL">St Barthélémy</option>
+                        <option value="SM">St Martin</option>
+                        <option value="WF">Wallis et Futunua</option>
+                        <option value="PF">Polynésie Française</option>
+                        <option value="NC">Nouvelle Calédonie</option>
+                      </optgroup>
+                      <option value="ZZ">Autre</option>
+                    </select>
+                  </div>
+                </div>
+                <button class="FormContainer-Submit" type="submit" name="valider">Rappel gratuit</button>
+              </section>
+              
+              
+            </form>
+
+          </div>
+
+        </article>
+      </div>
+    </div>
+  </section>
+
+
+
+  <?php include('include/footer_reassurance.php'); ?>
+
+  <p class="SiteCopyright">
+    <?php include('include/footer_copyright.php'); ?>
+  </p>
+
+  <!-- #### SCRIPTS #### -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="js/formValidator.js"></script>
   <script type="text/javascript" src="js/nameCompa.js"></script>
+
+  <!-- #### REMARKETINGS #### -->
+  <?php include('include/remarketing/adwords.php');
+              include('include/remarketing/analytics.php');
+              include('include/remarketing/facebook.php'); ?>
+
 </body>
+
 </html>
--- js/nameCompa.js
+++ js/nameCompa.js
@@ -2,124 +2,111 @@
  * Title:  Test Compatibilté des Prénom
  * Author: Mohamed Moughamir <hello@omnizya.com>
  * Copyright: KG-Com 2017
-*/
+ */
 /*
 global $
 */
+  var letters = {
+      A: 1,J: 1,S: 1,
+      B: 2,K: 2,T: 2,
+      C: 3,L: 3,U: 3,
+      D: 4,M: 4,V: 4,
+      E: 5,N: 5,W: 5,
+      F: 6,O: 6,X: 6,
+      G: 7,P: 7,Y: 7,
+      H: 8,Q: 8,Z: 8,
+      I: 9,R: 9
+    },
+    paths = {
+      'one': 'Entre vous et <b>{{OTHER}}</b>, c’est toute feu toute flamme ! Votre couple est la somme de vos deux personnalités, fortes, indépendantes et capables de se réaliser soi-même. Pour que votre histoire fonctionne, vos deux caractères doivent se compléter et chacun doit fournir des efforts très particuliers. Votre relation avec <b>{{OTHER}}</b> est à prendre avec des pincettes, mais le jeu en vaut vraiment la chandelle. <b>{{USER}}</b>, c’est en prenant du recul que vous avancerez efficacement…',
+      'two': 'A première vue, vous n’êtes pas vraiment faits pour vivre l’un avec l’autre. Mais votre volonté, combinée à celle de <b>{{OTHER}}</b>, attenue cette fausse impression. Tout est possible avec l’état d’esprit approprié! <b>{{USER}}</b>, allez-y pas à pas, en prenant soin de ne pas griller les étapes. Prenez le temps de réflexion nécessaire avant d’agir, sous peine de devoir faire face à bien des difficultés relationnelles.',
+      'three': 'Votre couple avec <b>{{OTHER}}</b> est marqué par les blocages ! Vous manquez tout deux de souplesse d’esprit et de qualités d’adaptation. Ce qui va vous causer bien des ennuis dès que la première nouveauté ou la première instabilité va pointer le bout de son nez dans votre couple. Vous aimez la stabilité et vous pourriez aisément l’obtenir, à condition de surmonter tous ces blocages. Une chose facile à faire en soi, mais surtout très utile, car vous et {{OTHER}} pourriez être promis à un bel avenir…'
+    },
+    user = '{{USER}}',
+    other = '{{OTHER}}',
+    names = {},
+    score = {},
+    name = $('.name').val(),
+    submit = $('.btn-submit'),
+    person = [];
 
-var letters = {
-  A:1, J:1, S:1,
-  B:2, K:2, T:2,
-  C:3, L:3, U:3,
-  D:4, M:4, V:4,
-  E:5, N:5, W:5,
-  F:6, O:6, X:6,
-  G:7, P:7, Y:7,
-  H:8, Q:8, Z:8,
-  I:9, R:9
-},
-paths = {
-  'one' : 'Entre vous et <b>{{OTHER}}</b>, c’est toute feu toute flamme ! Votre couple est la somme de vos deux personnalités, fortes, indépendantes et capables de se réaliser soi-même. Pour que votre histoire fonctionne, vos deux caractères doivent se compléter et chacun doit fournir des efforts très particuliers. Votre relation avec <b>{{OTHER}}</b> est à prendre avec des pincettes, mais le jeu en vaut vraiment la chandelle. <b>{{USER}}</b>, c’est en prenant du recul que vous avancerez efficacement…',
-  'two' : 'A première vue, vous n’êtes pas vraiment faits pour vivre l’un avec l’autre. Mais votre volonté, combinée à celle de <b>{{OTHER}}</b>, attenue cette fausse impression. Tout est possible avec l’état d’esprit approprié! <b>{{USER}}</b>, allez-y pas à pas, en prenant soin de ne pas griller les étapes. Prenez le temps de réflexion nécessaire avant d’agir, sous peine de devoir faire face à bien des difficultés relationnelles.',
-  'three' : 'Votre couple avec <b>{{OTHER}}</b> est marqué par les blocages ! Vous manquez tout deux de souplesse d’esprit et de qualités d’adaptation. Ce qui va vous causer bien des ennuis dès que la première nouveauté ou la première instabilité va pointer le bout de son nez dans votre couple. Vous aimez la stabilité et vous pourriez aisément l’obtenir, à condition de surmonter tous ces blocages. Une chose facile à faire en soi, mais surtout très utile, car vous et {{OTHER}} pourriez être promis à un bel avenir…'
-},
-user = '{{USER}}',
-other = '{{OTHER}}',
-names = {},
-score = {},
-name = $('.name').val(),
-submit = $('.btn-submit');
-var person = [];
-  
-function capitalize(str){
- return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}  
-  
-function simpleScore(name) {
-  var nameScore = 0;
-  
-  for (var i = 0; i < name.length; i++ ) {
-    
-    name = name.toUpperCase();
-    
-    var curChar = name.charAt(i);
-    var curValue = letters[curChar];
-    
-    var nameScore = nameScore + curValue;
-  
-    //console.info(nameScore);
-    //console.log(name);
-    //console.log(score[name]);
-    //console.log( "Total score for this name is: " + name + score[name] );
-    
-  }
-  
-  var singleDigitScore = nameScore;
-  
-  while( singleDigitScore >= 10 ){
-        var total = 0;
-        var str = '' + singleDigitScore;
-        for( var i = 0; i < str.length; i++ ){
-          total = total + parseInt( str.charAt(i) );
-        }
-        singleDigitScore = total;
-        score[name] = singleDigitScore;
-        
-        console.log(capitalize(name) + " Single Digit score is: " + singleDigitScore);
-      }
-  
-}
+$(document).ready(function() {
 
 
-submit.on('click', function(){
-  
-  $(".name").each(function() {
-    
-    names[$(this).attr("name")] = $(this).val();
-    
-    //console.log(names);
-    person.push($(this).val()); // person[i]
-    
-    simpleScore(names[$(this).attr("name")]);
-    
-    console.info('done');
-  });
-  
-  
-}); 
 
-/**
- * Find and Replace with regEx
-*/
-//function (search, replacement) {
-//    var target = this;
-//    return target.replace(new RegExp(search, 'g'), replacement);
-//};
-
-
-function findReplace(word, replace, text) {
-  var regex = '/'+word+'/gm';
-  var str = text;
-  var m;
-  
-  while ((m = regex.exec(str)) !== null) {
-    
-    if (m.index === regex.lastIndex) {
-      regex.lastIndex++;
-    }
-    
-    m.forEach((match, groupIndex) => {
-      console.log(`Found match, group ${groupIndex}: ${match}`);
+  function capitalize(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
   }
-  console.info('found string');
-  
-  
-  var personOne = person[0].toUpperCase();
-  var personTwo = person[1].toUpperCase();
+
+  function simpleScore(name) {
+    var nameScore = 0;
+
+    for (var i = 0; i < name.length; i++) {
+
+      name = name.toUpperCase();
+
+      var curChar = name.charAt(i);
+      var curValue = letters[curChar];
+
+      var nameScore = nameScore + curValue;
+    }
+
+    var singleDigitScore = nameScore;
+
+    while (singleDigitScore >= 10) {
+      var total = 0;
+      var str = '' + singleDigitScore;
+      for (var i = 0; i < str.length; i++) {
+        total = total + parseInt(str.charAt(i));
+      }
+      singleDigitScore = total;
+      score[name] = singleDigitScore;
+
+      console.log(capitalize(name) + " Single Digit score is: " + singleDigitScore);
+    }
+
+  }
+
+
+  submit.on('click', function() {
+    $(".step-1").addClass('hidden');
+    $(".resault").removeClass('hidden');
+    $(".FormContainer").addClass('scale');
+    $(".more").show();
     
-  var path =  (score[personOne] + score[personTwo]) / 2;
+    $(".name").each(function() {
+
+      names[$(this).attr("name")] = $(this).val();
+
+      //console.log(names);
+      person.push($(this).val()); // person[i]
+
+      simpleScore(names[$(this).attr("name")]);
+
+      console.info('done');
+    });
+
+
+  });
+
+  function findReplace(word, replaceText, text) {
     
+    var sRegExInput = new RegExp(word, 'g');
+    var textChange = text.replace(sRegExInput, replaceText);
+    
+    var textArea = $('.resault-text').text(); 
+    
+    textArea = $('.resault-text').html(textChange);
+
+
+
+    var personOne = person[0].toUpperCase();
+    var personTwo = person[1].toUpperCase();
+
+    var path = (score[personOne] + score[personTwo]) / 2;
+
     if (path <= 1) {
       // paths.one
       for (var i = 0; i < person.length; i++) {
@@ -128,7 +115,7 @@ function findReplace(word, replace, text) {
           case person[0]:
             findReplace(user, person[0], path.one);
             break;
-          
+
           case person[1]:
             findReplace(other, person[1], path.one);
             break;
@@ -137,15 +124,16 @@ function findReplace(word, replace, text) {
         }
         console.log('Get path one');
       }
-      
-    } else if (path > 1 && path <= 2) {
+
+    }
+    else if (path > 1 && path <= 2) {
       for (var i = 0; i < person.length; i++) {
         //person[i]
         switch (true) {
           case person[0]:
             findReplace(user, person[0], path.two);
             break;
-          
+
           case person[1]:
             findReplace(other, person[1], path.two);
             break;
@@ -154,14 +142,15 @@ function findReplace(word, replace, text) {
         }
         console.log('Get path two');
       }
-    } else{
+    }
+    else {
       for (var i = 0; i < person.length; i++) {
         //person[i]
         switch (true) {
           case person[0]:
             findReplace(user, person[0], path.three);
             break;
-          
+
           case person[1]:
             findReplace(other, person[1], path.three);
             break;
@@ -171,4 +160,8 @@ function findReplace(word, replace, text) {
         console.log('Get path three');
       }
     }
-}
+  }
+
+
+  console.info('Loaded');
+});
