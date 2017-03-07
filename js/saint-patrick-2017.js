4b43f4be4c775d625371c5ba542b49231cd7a4eb
MOUGHAMIR Mohamed <moughamir@gmail.com>
2017-03-07 09:24:20 +0000

    [+] New Mixins [+] The game have a new effect [+] Now we import our libs and modules from JsDilvr cdn

--- js/saint-patrick-2017.js
+++ js/saint-patrick-2017.js
@@ -1,28 +1,23 @@
-// TODO: replace timer with an effect (cell shine randomly)
-/* global angular */
-/* global $ */
+// Main Application
+/*global angular*/
+/*global $*/
+
 'use strict';
-// Globals
-var currentSessionOpen = false,
-  previousCard = null,
-  numPairs = 0;
-
-var app = angular.module('MyAstro', ['ngAnimate']);
-
+var app = angular.module('myAstro', ['ngAnimate']);
 
 function GameController($scope, $timeout) {
-
-  $scope.deck = app.createDeck();
-  $scope.isGuarding = true;
+  $scope.deck = createDeck();
+  $scope.isGuarding = false;
   $scope.inGame = false;
+  $scope.shine = shine();
 
   $scope.check = function(card) {
-
     if (currentSessionOpen && previousCard != card && previousCard.item == card.item && !card.isFaceUp) {
       card.isFaceUp = true;
       previousCard = null;
       currentSessionOpen = false;
       numPairs++;
+      shine(true);
     }
     else if (currentSessionOpen && previousCard != card && previousCard.item != card.item && !card.isFaceUp) {
       $scope.isGuarding = true;
@@ -33,6 +28,7 @@ function GameController($scope, $timeout) {
         previousCard = null;
         $scope.isGuarding = $scope.timeLimit ? false : true;
       }, 1000);
+      shine(false);
     }
     else {
       card.isFaceUp = true;
@@ -41,18 +37,46 @@ function GameController($scope, $timeout) {
     }
 
     if (numPairs == constants.getNumMatches()) {
-      $scope.stopTimer();
+      shine(true);
       $('#modal').modal('toggle');
     }
+  }; //end of check()
 
-  }; // check()
-  // remember we don't need timer in this version
-  $scope.timeLimit = 60000; // 60sec
+  // for the timer
+  $scope.timeLimit = 60000;
   $scope.isCritical = false;
+
   var timer = null;
 
-  $scope.start = function() {};
-  $scope.stopTimer = function() {};
+  // start the timer as soon as the player presses start
+  $scope.start = function() {
+    // I need to fix this redundancy. I initially did not create this
+    // game with a start button.
+    $scope.deck = createDeck();
+    // set the time of 1 minutes and remove the cards guard
+    $scope.timeLimit = 60000;
+    $scope.isGuarding = false;
+    $scope.inGame = true;
+
+    ($scope.startTimer = function() {
+      $scope.timeLimit -= 1000;
+      $scope.isCritical = $scope.timeLimit <= 10000 ? true : false;
+
+      timer = $timeout($scope.startTimer, 1000);
+      if ($scope.timeLimit === 0) {
+        $scope.stopTimer();
+        $scope.isGuarding = true;
+      }
+    })();
+  };
+  // function to stop the timer
+  $scope.stopTimer = function() {
+    $timeout.cancel(timer);
+    $scope.inGame = false;
+    previousCard = null;
+    currentSessionOpen = false;
+    numPairs = 0;
+  };
 
 }
 
@@ -62,17 +86,13 @@ function ModalController($scope) {
     $scope.showModal = !$scope.showModal;
   };
 }
-
-// injection
 GameController.$inject = ["$scope", "$timeout"];
 ModalController.$inject = ["$scope"];
 
 app.controller('ModalController', ModalController);
 app.directive('modal', function() {
-
   return {
-
-    template: '<aside class="modal fade" id="modal">' +
+    template: '<div class="modal fade" id="modal">' +
       '<div class="modal-dialog">' +
       '<div class="modal-Container">' +
       '<div class="modal-content">' +
@@ -80,98 +100,84 @@ app.directive('modal', function() {
       '</div>' +
       '</div>' +
       '</div>' +
-      '</aside>',
+      '</div>',
     restrict: 'E',
     transclude: true,
     replace: true,
     scope: true,
     link: function postLink(scope, element, attrs) {
-
       scope.title = attrs.title;
 
       scope.$watch(attrs.visible, function(value) {
-
-        if (value == true) {
+        if (value == true)
           $(element).modal('show');
-        }
-        else {
+        else
           $(element).modal('hide');
-        }
-
       });
 
       $(element).on('shown.bs.modal', function() {
-
         scope.$apply(function() {
           scope.$parent[attrs.visible] = true;
         });
-
       });
 
       $(element).on('hidden.bs.modal', function() {
-
         scope.$apply(function() {
           scope.$parent[attrs.visible] = false;
         });
-
       });
-
     }
   };
-
 });
 
+// constant variables
 var constants = new(function() {
   var rows = 4;
   var columns = 4;
   var numMatches = (rows * columns) / 2;
-
   this.getRows = function() {
     return rows;
   };
-
   this.getColumns = function() {
     return columns;
   };
-
   this.getNumMatches = function() {
     return numMatches;
   };
-
 })();
 
+// Global Variables
+var currentSessionOpen = false;
+var previousCard = null;
+var numPairs = 0;
 
+// this function creates deck of cards that returns an object of cards
+// to the caller
 function createDeck() {
+  var rows = constants.getRows();
+  var cols = constants.getColumns();
+  var key = createRandom();
+  var deck = {};
+  deck.rows = [];
 
-  var rows = constants.getRows(),
-    cols = constants.getColumns(),
-    key = createRandom(),
-    deck = {},
-    deck.rows = [];
-
+  // create each row
   for (var i = 0; i < rows; i++) {
+    var row = {};
+    row.cards = [];
 
-    var row = {},
-      row.cards = [];
-
+    // creat each card in the row
     for (var j = 0; j < cols; j++) {
-
       var card = {};
       card.isFaceUp = false;
       card.item = key.pop();
       row.cards.push(card);
-
     }
-
     deck.rows.push(row);
-
   }
-
   return deck;
-
 }
 
-
+// used to remove something form an array by index
 function removeByIndex(arr, index) {
   arr.splice(index, 1);
 }
@@ -180,52 +186,76 @@ function insertByIndex(arr, index, item) {
   arr.splice(index, 0, item);
 }
 
+// creates a random array of items that contain matches
+// for example: [1, 5, 6, 5, 1, 6]
 function createRandom() {
+  var matches = constants.getNumMatches();
+  var pool = [];
+  var answers = [];
+  var astro = ['belier', 'cancer', 'capricorne', 'gemeaux',
+    'poisson', 'taureau', 'verseau', 'vierge'
+  ];
+  // set what kind of item to display
+  var items = astro;
 
-  var matches = constants.getNumMatches(),
-    pool = [],
-    answers = [],
-    astro = [
-      'belier', 'cancer', 'capricorn', 'gemeaux',
-      'poisson', 'taureau', 'verseau', 'vierge'
-    ];
-
+  // create the arrays for random numbers and item holder
   for (var i = 0; i < matches * 2; i++) {
-    pool.push[i];
+    pool.push(i); // random numbers
   }
 
-
-  // generate an array with random items
-  // TODO: Add new feature: pick cells randomly and animate them
-
+  // generate an array with the random items
   for (var n = 0; n < matches; n++) {
-
-    // grab random item for 'astro' array and remove that item from the 
+    // grab random letter from array and remove that letter from the
     // original array
-    var randItem = Math.floor((Math.random() * astro.length));
-    var item = astro[randItem];
-    removeByIndex(astro, randItem);
-
+    var randLetter = Math.floor((Math.random() * items.length));
+    var letter = items[randLetter];
+    removeByIndex(items, randLetter);
     // generate two random placements for each item
     var randPool = Math.floor((Math.random() * pool.length));
 
     // remove the placeholder from answers and insert the letter into
     // random slot
-    insertByIndex(answers, pool[randPool], item);
+    insertByIndex(answers, pool[randPool], letter);
 
     // remove random number from pool
     removeByIndex(pool, randPool);
 
-    // redo this proccess for the secon placement
+    // redo this process for the second placement
     randPool = Math.floor((Math.random() * pool.length));
-    insertByIndex(answers, pool[randPool], item);
+    insertByIndex(answers, pool[randPool], letter);
 
     // remove rand number from pool
     removeByIndex(pool, randPool);
   }
-
   return answers;
 }
 
 
-app.controller('GameController', GameController);
+
+
+function shine(state) {
+  console.log(state);
+  if (state == true) {
+    console.log(state + ' 2');
+    setInterval(
+      function() {
+        randomShine();
+      }, 300
+    );
+  }
+  else {
+    clearInterval(1);
+  }
+
+}
+
+function randomShine() {
+  var cards = $('.card');
+
+  var cardId = Math.floor(Math.random() * cards.length);
+  cards.eq(cardId).addClass('shine');
+  setTimeout(function() {
+    cards.eq(cardId).removeClass('shine');
+  }, 300);
+}
+app.controller("GameController", GameController);
