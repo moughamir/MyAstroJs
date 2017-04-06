29bdc89ddb636825c12bd5ded89ff2a7e4e2b3a4
Laurène Dourdin <laurene.kgcom@gmail.com>
2017-04-06 19:19:19 +0200

    Some fixes & cleanings

--- myastro-tchat-dri.php
+++ myastro-tchat-dri.php
@@ -1,43 +1,26 @@
 <?php
-    /**
-     * Tchat My Astro DRi
-     * 
-     * Created on : Apr 5th, 2017
-     * Author     : Mohamed Moughamir <hello@omnizya.com>
-     */
-    include('include/process-dri.php');
-    $assets = 'images_landing/tchat';
-    $pageName = 'myastro-tchat-dri';
-    $site   = getenv('MYASTRO_ROOT_URL');
+/**
+ * myastro-tchat-dri
+ * -----------------
+ * Created on : 05 avril 2017 By Mohamed Moughamir <hello@omnizya.com> 
+ */
+include('include/process-dri.php');
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
-
     <head>
-        <meta charset="UTF-8">
+        <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="">
         <meta name="robots" content="noindex, nofollow" />
-        <meta property="fb:app_id" content="1276526482364681" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="DRI Tchat MyAstro" />
-        <meta property="og:url" content="http://<?= $site ?>/tchat-ma" />
-        <meta property="og:image" content="http://<?= $site . '/' . $assets ?>/cover.jpg" />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="675" />
-        <meta property="og:description" content="" />
-        <meta property="og:locale" content="fr_FR" />
         <title>Tchat MyAstro</title>
-        <link rel="icon" type="image/png" href="<?= $assets ?>/favicon.png" />
-        <link rel="stylesheet" href="css/myastro-miniform.css" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Open+Sans" rel="stylesheet">
+        <link rel="icon" type="image/png" href="logo_myastro_tchat_32x32.png" />
+        <link rel="stylesheet" href="css/myastro-tchat.css" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Open+Sans" rel="stylesheet" />
         <!--[if lt IE 9]>
         <script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3,respond@1.4.2"></script>
         <![endif]-->
     </head>
-
     <body class="main">
         <div class="overlay" id="form-overlay"></div>
         <header class="SiteHeader">
@@ -50,80 +33,80 @@
         </div>
         <main class="ContentBand">
             <div class="PageWrapper fullview">
-                <article class="FormContainer DRI-Form">
-                    <div class="Fields-Table">
-                        <?php if($state == 'MAIL_SENT' || $state == 'MAIL_ALREADY_SENT'){ ?>
+                <article class="DRI-Form">
+                    <?php if($state == 'MAIL_SENT' || $state == 'MAIL_ALREADY_SENT'){ ?>
                     <p class="message">
                         Merci, votre demande a bien été prise en compte.<br/>
-                        <strong class="highlight">Un voyant vous recontactera dans quelques instants</strong>.
+                        <strong class="highlight">Un voyant vous recontactera dans quelques instants.</strong>
                     </p>                        
-                <?php } else { ?>
-                    <?php if(!empty($err) || $state == 'MAIL_ERROR'){ ?>
-                <p class="alert alert-danger">
-                    <b><i class="fa fa-warning"></i> Une erreur est survenue.</b><br>
-                        <?php foreach($err as $msg){
-                            echo $msg.'<br>';
-                        } ?>
-                </p>
-                    <?php } ?>
-                        <h2 class="DRI-Form-Title"> Vous avez déjà bénéficié de l’offre :<br><strong>&#8220; 3 minutes de tchat gratuit &#8221;</strong></h2>
-                        <p> Vous pourrez de nouveau profiter de cette offre dans 7 jours.<br>En attendant, vous pouvez consulter un voyant immédiatement par téléphone.</p>
-                        <h3 class="DRI-Form-Subtitle">Faites-vous rappeler gratuitement en remplissant le formulaire :</h3>
-                        <p><strong>Nos voyants répondent à vos questions</strong></p>
-                        <form class="DRI-FormContainer" method="post">
-                            <div class="FormContainer-Fields">
-                                <div class="Fields-Table-Row"> <label for="name" class="FormField-Label">Mon prénom</label>
-                                    <div class="FormField"> <input type="text" id="name" name="prenom" placeholder="Mon Prénom" class="FormField-Input" value="<?= $prenom ?>" required /> </div>
-                                </div>
-                                <div class="Fields-Table-Row"> <label for="tel" class="FormField-Label">Mon numéro de téléphone</label>
-                                    <div class="FormField"> <input type="tel" id="tel" name="tel" placeholder="Mon N° de téléphone" class="FormField-Input" value="<?= $tel ?>" required /> </div>
-                                </div>
-                                <div class="Fields-Table-Row"> <label for="pays" class="FormField-Label">Mon pays</label>
-                                    <div class="FormField"> <select name="pays" id="pays" class="FormField-Input" required>
-                                    <option value="" selected>Votre Pays</option>
-                                    <option value="BE">Belgique</option>
-                                    <option value="CA">Canada</option>
-                                    <option value="LU">Luxembourg</option>
-                                    <option value="CH">Suisse</option>
-                                    <option value="FR" selected>France Métropolitaine</option>
-                                    <optgroup label="DOM-TOM">
-                                        <option value="MQ">Martinique</option>
-                                        <option value="GP">Guadeloupe</option>
-                                        <option value="GF">Guyane</option>
-                                        <option value="RE">La Réunion</option>
-                                        <option value="YT">Mayotte</option>
-                                        <option value="PM">St Pierre et Miquelon</option>
-                                        <option value="BL">St Barthélémy</option>
-                                        <option value="SM">St Martin</option>
-                                        <option value="WF">Wallis et Futunua</option>
-                                        <option value="PF">Polynésie Française</option>
-                                        <option value="NC">Nouvelle Calédonie</option>
-                                    </optgroup>
-                                    <option value="ZZ">Autre</option>
-                                </select> </div>
-                                </div>
+                    <?php } else { ?>
+                        <?php if(!empty($err) || $state == 'MAIL_ERROR'){ ?>
+                    <p class="alert alert-danger">
+                        <b><i class="fa fa-warning"></i> Une erreur est survenue.</b><br>
+                            <?php foreach($err as $msg){
+                                echo $msg.'<br>';
+                            } ?>
+                    </p>
+                        <?php } ?>
+                    <header class="DRI-Form-Title">
+                        Vous avez déjà bénéficié de l’offre :<br>
+                        <strong>« 5 minutes de tchat gratuit »</strong>
+                    </header>
+                    <p>
+                        Vous pourrez de nouveau profiter de cette offre dans 7 jours.<br>
+                        En attendant, vous pouvez consulter un voyant immédiatement par téléphone.
+                    </p>
+                    <h3 class="DRI-Form-Subtitle">Faites-vous rappeler gratuitement en remplissant le formulaire :</h3>
+                    <p><strong>Nos voyants répondent à vos questions.</strong></p>
+                    <form class="DRI-FormContainer" method="post">
+                        <div class="FormContainer-Fields">
+                            <div class="Fields-Table-Row"> <label for="name" class="FormField-Label">Mon prénom</label>
+                                <div class="FormField"> <input type="text" id="name" name="prenom" placeholder="Mon Prénom" class="FormField-Input" value="<?= $prenom ?>" required /> </div>
                             </div>
-                            <h1 class="DRI-Slogan">Vos 10 premières minutes <span class="cap">gratuites</span> </h1>
-                            <!-- SUBMIT --><button class="FormContainer-Submit btn-rose" type="submit" name="demande_rappel">Rappel Gratuit</button> </form>
-                        <?php } ?> </div>
+                            <div class="Fields-Table-Row"> <label for="tel" class="FormField-Label">Mon numéro de téléphone</label>
+                                <div class="FormField"> <input type="tel" id="tel" name="tel" placeholder="Mon N° de téléphone" class="FormField-Input" value="<?= $tel ?>" required /> </div>
+                            </div>
+                            <div class="Fields-Table-Row"> <label for="pays" class="FormField-Label">Mon pays</label>
+                                <div class="FormField"> <select name="pays" id="pays" class="FormField-Input" required>
+                                <option value="" selected>Votre Pays</option>
+                                <option value="BE">Belgique</option>
+                                <option value="CA">Canada</option>
+                                <option value="LU">Luxembourg</option>
+                                <option value="CH">Suisse</option>
+                                <option value="FR" selected>France Métropolitaine</option>
+                                <optgroup label="DOM-TOM">
+                                    <option value="MQ">Martinique</option>
+                                    <option value="GP">Guadeloupe</option>
+                                    <option value="GF">Guyane</option>
+                                    <option value="RE">La Réunion</option>
+                                    <option value="YT">Mayotte</option>
+                                    <option value="PM">St Pierre et Miquelon</option>
+                                    <option value="BL">St Barthélémy</option>
+                                    <option value="SM">St Martin</option>
+                                    <option value="WF">Wallis et Futunua</option>
+                                    <option value="PF">Polynésie Française</option>
+                                    <option value="NC">Nouvelle Calédonie</option>
+                                </optgroup>
+                                <option value="ZZ">Autre</option>
+                            </select> </div>
+                            </div>
+                        </div>
+                        <h1 class="DRI-Slogan">Vos 10 premières minutes <span class="cap">gratuites</span> </h1>
+                        <!-- SUBMIT -->
+                        <button class="FormContainer-Submit btn-rose" type="submit" name="demande_rappel">Rappel Gratuit</button>
+                    </form>
+                    <?php } ?>
                 </article>
             </div>
         </main>
         <footer>
-            <p class="SiteCopyright"><?php include('include/footer_copyright.php'); ?> </p>
-            <script src="https://cdn.jsdelivr.net/g/lodash@4.17.4,jquery@3.1.1"></script>
-            <!-- #### REMARKETINGS #### -->
-            <?php
-                include('include/remarketing/adwords.php');
-                include('include/remarketing/analytics.php');
-                include('include/remarketing/facebook.php');
-            ?>
-                <!-- #### CONVERSION #### -->
-            <?php
-                include('include/conversion/adwords.php');
-                include('include/conversion/facebook.php');
-            ?>
+            <p class="SiteCopyright"><?php include('include/footer_copyright.php');?> </p>
         </footer>
+        <!-- #### SCRIPTS #### -->
+<!--        <script src="https://cdn.jsdelivr.net/g/lodash@4.17.4,jquery@3.1.1"></script>-->
+        <!-- #### REMARKETINGS #### -->
+        <?php include('include/remarketing/adwords.php');
+              include('include/remarketing/analytics.php');
+              include('include/remarketing/facebook.php');?>
     </body>
-
 </html>
--- myastro-tchat-rc.php
+++ myastro-tchat-rc.php
@@ -1,46 +1,34 @@
 <?php
 /**
- * pouvoir-des-trois == AFFILBASE
- * 
- * Created on : Feb 13th, 2017
- * Author     : Mohamed Moughamir <hello@omnizya.com>
+ * myastro-tchat-rc == REFLEXCASH
+ * ------------------------------
+ * Created on : 04 avril 2017 By Mohamed Moughamir <hello@omnizya.com> 
  */
 session_start();
 
-$assets = 'images_landing/tchat';
 $pageName = "myastro-tchat-rc";
 $method = "general-suscribe";
 $support = "voyance";
 $site = "myastro.fr";
-$reflex = "reflexcache";
+$source = "reflexcache";
 $question = ['code' => 'tchatmyastro_1', 'subject' => 'evenement', 'text' => 'Campagne Tchat mini formulaire'];
+
 $_SESSION['reflexcash_transactionid'] = isset($_GET['t1']) ? $_GET['t1'] : '';
 $_SESSION['reflexcash_affiliateid'] = isset($_GET['t2']) ? $_GET['t2'] : '';
 $_SESSION['reflexcash_source'] = isset($_GET['t3']) ? $_GET['t3'] : '';
 $prenom = isset($_GET['prenom']) ? $_GET['prenom'] : '';
 $email = isset($_GET['email']) ? $_GET['email'] : '';
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
     <head>
-        <meta charset="UTF-8">
+        <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="">
         <meta name="robots" content="noindex, nofollow" />
-        <meta property="fb:app_id" content="1276526482364681" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Tchat MyAstro" />
-        <meta property="og:url" content="http://<?= $site ?>/tchat-ma" />
-        <meta property="og:image" content="http://<?= $site . '/' . $assets ?>/cover.jpg" />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="675" />
-        <meta property="og:description" content=""/>
-        <meta property="og:locale" content="fr_FR" />
         <title>Tchat MyAstro</title>
-        <link rel="icon" type="image/png" href="<?= $assets ?>/favicon.png" />
-        <link rel="stylesheet" href="css/myastro-miniform.css" type="text/css" />
+        <link rel="icon" type="image/png" href="logo_myastro_tchat_32x32.png" />
+        <link rel="stylesheet" href="css/myastro-tchat.css" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Open+Sans" rel="stylesheet">
         <!--[if lt IE 9]>
         <script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3,respond@1.4.2"></script>
@@ -58,25 +46,23 @@ $email = isset($_GET['email']) ? $_GET['email'] : '';
         </div>
         <main class="ContentBand">
             <div class="PageWrapper fullview">
-
                 <form id="form-container" class="ajax">
                     <!-- ########## identification formulaire ########## -->
-                    <input type="hidden" name="source" value="<?= $pageName; ?>" />
-                    <input type="hidden" name="method" value="<?= $method; ?>" />
-                    <input type="hidden" name="support" value="<?= $support; ?>" />
-                    <input type="hidden" name="site" value="<?= $site; ?>" />
-                    <input type="hidden" name="affiliation" value="<?= $reflex; ?>" />
+                    <input type="hidden" name="source" value="<?= $pageName;?>" />
+                    <input type="hidden" name="method" value="<?= $method;?>" />
+                    <input type="hidden" name="support" value="<?= $support;?>" />
+                    <input type="hidden" name="site" value="<?= $site;?>" />
+                    <input type="hidden" name="affiliation" value="<?= $source;?>" />
                     <input type="hidden" name="dri" value="myastro/offre-gratuite" />
-                    <input type="hidden" name="optional_birthdate" value="true" />
                     <!-- ########## autres champs pré-remplis ######### -->
                     <input type="hidden" name="cguv" value="1" />
                     <input type="hidden" name="partenaires" value="1" />
-                    <input type="hidden" name="question_code" value="<?= str_replace('"', "'", json_encode($question)) ?>" />
+                    <input type="hidden" name="question_code" value="<?= str_replace('"', "'", json_encode($question));?>" />
+                    <input type="hidden" name="optional_birthdate" value="true" />
                     <input type="hidden" name="convertir" value="1" />
                     <!-- ############################################### -->
-
                     <section class="Merci-Form">
-                        <header class="Merci-Form--header">
+                        <header class="Merci-Form-Header">
                             <h2><span class="cap">5 minutes</span> de Tchat <span class="cap">gratuites</span></h2>
                         </header>
                         <div class="FormContainer">
@@ -97,38 +83,34 @@ $email = isset($_GET['email']) ? $_GET['email'] : '';
                             <div class="Fields-Table-Row">
                                 <label for="name" class="FormField-Label hidden">Votre prénom</label>
                                 <div class="FormField">
-                                    <input id="name" type="text" name="prenom" class="FormField-Input" placeholder="Votre prénom" required />
+                                    <input id="name" type="text" name="prenom" class="FormField-Input" placeholder="Votre prénom" value="<?= $prenom;?>" required />
                                 </div>
                             </div>
                             <!--eMail-->
                             <div class="Fields-Table-Row">
                                 <label for="email" class="FormField-Label hidden">Votre email</label>
                                 <div class="FormField">
-                                    <input id="email" type="email" name="email" class="FormField-Input" placeholder="Votre Email" required />
+                                    <input id="email" type="email" name="email" class="FormField-Input" placeholder="Votre Email" value="<?= $email;?>" required />
                                 </div>
                             </div>
-                            <button class="FormContainer-Submit cta-chat" type="submit" name="valider">Je Tchat!</button>
+                            <button class="FormContainer-Submit cta-chat" type="submit" name="valider">Je Tchat !</button>
                         </div>
                     </section>
                 </form>
             </div>
-        </div>
-    </main>
-    <footer>
-        <p class="SiteCopyright">
-            <?php include('include/footer_copyright.php'); ?>
-        </p>
+        </main>
+        <footer>
+            <p class="SiteCopyright">
+                <?php include('include/footer_copyright.php');?>
+            </p>
+        </footer>
         <!-- #### SCRIPTS #### -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="js/formValidator.js"></script>
         <script src="https://cdn.jsdelivr.net/g/lodash@4.17.4,jquery@3.1.1"></script>
-        <!--script type="text/javascript" src="js/pouvoir-des-trois.js"></script-->
         <!-- #### REMARKETINGS #### -->
-        <?php
-        include('include/remarketing/adwords.php');
-        include('include/remarketing/analytics.php');
-        include('include/remarketing/facebook.php');
-        ?>
-    </footer>
-</body>
+        <?php include('include/remarketing/adwords.php');
+              include('include/remarketing/analytics.php');
+              include('include/remarketing/facebook.php');?>
+    </body>
 </html>
