4a102d07609c36f923ae742fc3b4dc948ffa895f
sanadi <sanadi>
2017-08-16 16:36:12 +0100

    online consultation

---
+++ rdv_web/client_web.php
@@ -0,0 +1,199 @@
+<?php // FICHIER INCLUS DEPUIS inc/ajaxHandler.php
+
+/* ========================================================================== *
+ *                               INITIALISATION                               *
+ * ========================================================================== */
+$bdd = new bdd(DBLOGIN, DBPASS, DBNAME, DBHOST);
+$kgestion = new APIKGestion;
+
+$err = array();
+$conversion = 0;
+$retour = array();
+$reinscription = false;
+
+$today_date_bdd = date('Y-m-d');
+$today_datetime_bdd = date('Y-m-d H:i:s');
+$today_date_smf = date('m/d/Y');
+
+/* ========================================================================== *
+ *                     TRAITEMENT DES DONNÉES DE TRACKING                     *
+ * ========================================================================== */
+
+$id_rdm  = uuid();
+$ip      = $_SERVER['REMOTE_ADDR'];
+$page    = explode("?", $_SERVER['HTTP_REFERER'])[0];
+$website = isset($param['site']) ? $param['site'] : '';
+$source  = isset($param['affiliation']) ? $param['affiliation'] : false;
+$formurl_kgs = isset($param['source']) ? $param['source'] : false;
+$regformurl_kgs = $formurl_kgs;
+$gclid   = isset($param['gclid']) ? $param['gclid'] : '';
+$voyant  = isset($param['voyant']) ? $param['voyant'] : '';
+// ---- TRACKING REFLEX
+$rc_affiliateid = isset($_SESSION['reflexcash_affiliateid']) ? $_SESSION['reflexcash_affiliateid'] : '';
+$rc_source = isset($_SESSION['reflexcash_source']) ? $_SESSION['reflexcash_source'] : '';
+
+if(!$website){
+    $website = 'myastro.fr';
+}
+if(!$source){
+    $source = 'naturel';
+}
+if(!$formurl_kgs){
+    $err['sys'] = 'Système indisponible, veuillez réessayer plus tard.';
+}
+
+/* ========================================================================== *
+ *                        TRAITEMENT DE L'UTILISATEUR                         *
+ * ========================================================================== */
+
+// Genre -----------------------------------------------------------------------
+$sexe = isset($param['sexe']) ? $param['sexe'] : null;
+$sexe = str_replace('homme', 'M', $sexe);
+$sexe = str_replace('femme', 'F', $sexe);
+
+// Prénom ----------------------------------------------------------------------
+$prenom = form_firstname($err, $param);
+// Date de naissance & Signe Astrologique --------------------------------------
+$dtn_bdd = $dtn_smf = $dtn_ses = '';
+$need_birthdate = isset($param['optional_birthdate']) ? false : true;
+$dtn_j = isset($param['jour'])  && !empty($param['jour'])  ? $param['jour']  : false;
+$dtn_m = isset($param['mois'])  && !empty($param['mois'])  ? $param['mois']  : false;
+$dtn_a = isset($param['annee']) && !empty($param['annee']) ? $param['annee'] : false;
+$signe = '';
+
+if($need_birthdate){
+    if($dtn_j && $dtn_m && $dtn_a){
+        $dtn_bdd = $dtn_a."-".$dtn_m."-".$dtn_j; // FORMAT BDD MYASTRO
+        $dtn_smf = $dtn_m."/".$dtn_j."/".$dtn_a; // FORMAT SMARTFOCUS
+        $dtn_ses = $dtn_j."/".$dtn_m."/".$dtn_a; // FORMAT SESSION
+
+        $signe = get_signe_astro($dtn_j, $dtn_m);
+    } else {
+        $err['date_naissance'] = 'Merci dʼindiquer votre date de naissance.';
+    }
+}
+
+// Adresse mail ----------------------------------------------------------------
+$email = isset($param['email']) ? trim($param['email']) : false;
+if(!preg_match("$[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,4}$", $email)){
+    $err['email'] = 'Cette adresse email nʼest pas valide.';
+}
+
+// Numéro de téléphone & Pays --------------------------------------------------
+$partenaires = isset($param['partenaires']) ? 1 : 0;
+$horoscope   = isset($param['horoscope']) && !empty($param['horoscope']) ? 1 : 0;
+$param["pays"] = "France";
+list($tel, $pays) = form_phone($err, $param);
+
+// Question posée --------------------------------------------------------------
+if (isset($param['question_code'])){
+    // Nouveau champs de question avec texte de la question (value json)
+    if(!empty($param['question_code'])){
+        $question = json_decode(str_replace("'", '"', $param['question_code']), true);
+        if($question == 'AMOUR-CONJOINT'){
+            // En fonction de si le conjoint est rempli, soit amour, soit avour avec P2
+            $questions = array(
+                'question_1' => [ 'code' => 'question_1', 'subject' => 'sentimental', 'text' => 'Mon avenir sentimental - célibataire', 'conjoint' => false ],
+                'question_2' => [ 'code' => 'question_2', 'subject' => 'sentimental', 'text' => 'Mon avenir sentimental - en couple', 'conjoint' => true ],
+            );
+            $code = (isset($param['conjoint']) && !empty($param['conjoint'])) ? 'question_2' : 'question_1';
+            $question = $questions[$code];
+        } else {
+            $question['conjoint'] = isset($question['conjoint']) ? $question['conjoint'] : false;
+        }
+        $question['content'] = isset($param['question_content']) ? $param['question_content'] : null;
+    } else {
+        $err['question_code'] = 'Veuillez choisir votre question : Quel est le sujet de vos tourments ?';
+    }
+} else {
+    // Anciens champs de question
+    $question['code']    = isset($param['theme_id']) && !empty($param['theme_id']) ? $param['theme_id'] : false;
+    $question['subject'] = 'sentimental';
+    $question['content'] = isset($param['question']) && !empty($param['question']) ? $param['question'] : null;
+
+    if ($question['code'] == 'question_3' || $question['code'] == 'question_4'){
+        $question['subject'] = 'professionel';
+    }
+    if($question['code'] == 'question_73'){
+        $question['subject'] = 'financier';
+    }
+
+    $question['text'] = '';
+
+    // Tableau comprenant les questions qui nécessitent les infos du conjoint.
+    $questionConjoint = array('question_2', 'question_24', 'question_11', 'printemps16_amour');
+    $question['conjoint'] = in_array($question['code'], $questionConjoint);
+
+    if(!$question['code']){
+        $err['theme_id'] = 'Veuillez choisir votre question : Quel est le sujet de vos tourments ?';
+    }
+
+    $mind = isset($param['mind']) && !empty($param['mind']) ? $param['mind'] : false;
+    $user_responses = array(
+        'choix'     => $question['code'],
+        'question'  => $question['content'],
+        'situation' => $mind
+    );
+}
+/* ========================================================================== *
+ *                           TRAITEMENT DU CONJOINT                           *
+ * ========================================================================== */
+
+$conjoint_prenom  = $conjoint_dtn_bdd = $conjoint_dtn_smf = $conjoint_dtn_ses = $conjoint_signe = '';
+
+/* ========================================================================== *
+ *                          ENREGISTREMENT KGESTION                          *
+ * ========================================================================== */
+
+if(empty($err)){
+
+    $post_data = array(
+        'email'             => $email,
+        'firstName'         => $prenom,
+        'gender'            => $sexe,
+        'birthday'          => $dtn_bdd,
+        'sign'              => $signe,
+        'phone'             => $tel,
+        'country'           => $pays,
+        'spouseName'        => $conjoint_prenom,
+        'spouseSign'        => $conjoint_signe,
+        'spouseBirthday'    => $conjoint_dtn_bdd,
+        'questionDate'      => $today_date_bdd,
+        'questionSubject'   => $question['subject'],
+        'questionCode'      => $question['code'],
+        'questionText'      => $question['text'],
+        'questionContent'   => $question['content'],
+        'isOptinNewsletter' => $horoscope,
+        'isOptinPartner'    => $partenaires,
+        'myastroIp'         => $ip,
+        'myastroWebsite'    => $website,
+        'myastroSource'     => $source,
+        'myastroUrlOrigin'  => $formurl_kgs,
+        'myastroUrlRegistration' => $regformurl_kgs,
+        'myastroGclid'      => $gclid,
+        'reflexAffilateId'  => $rc_affiliateid,
+        'reflexSource'      => $rc_source,
+         );
+
+
+    if($reinscription){
+        $kgestion_id = $user->kgestion_id;
+        $kgestion_update = $kgestion->updateUser($kgestion_id, $post_data);
+        if (!$kgestion_update->success){
+        }
+    } else {
+        $kgestion_id = false;
+        $kgestion_insert = $kgestion->insertUserDRI($post_data);
+
+        if(!$kgestion_insert->success){
+            $err['sys'] = 'Système indisponible, veuillez réessayer plus tard.';
+        } else {
+            $kgestion_id = $kgestion_insert->id;
+            die(json_encode(array('client_id' => $kgestion_id)));
+
+        }
+    }
+
+} else {
+    die(json_encode(array('error'=>$err)));
+}
---
+++ rdv_web/consultation_web.php
@@ -0,0 +1,90 @@
+<?php // FICHIER INCLUS DEPUIS inc/ajaxHandler.php
+
+/* ========================================================================== *
+ *                               INITIALISATION                               *
+ * ========================================================================== */
+$bdd = new bdd(DBLOGIN, DBPASS, DBNAME, DBHOST);
+$kgestion = new APIKGestion;
+
+$err = array();
+$retour = array();
+
+
+/* ========================================================================== *
+ *                          RECUPERATION DES INFOS  COORDONNEE BANCAIRE                         *
+ * ========================================================================== */
+
+$carte_prenom = isset($param['carte_prenom']) ? $param['carte_prenom'] : '';
+$carte_nom = isset($param['carte_nom']) ? $param['carte_nom'] : '';
+$carte_num = isset($param['carte_num']) ? $param['carte_num'] : '';
+$crypto = isset($param['crypto']) ? $param['crypto'] : '';
+$expiration_mois = isset($param['expiration_mois']) ? $param['expiration_mois'] : '';
+$expiration_annee = isset($param['expiration_annee']) ? $param['expiration_annee'] : '';
+$client_id = isset($param['client_id']) ? $param['client_id'] : '';
+$planning_horaire_selection = isset($param['planning_horaire_selection']) ? $param['planning_horaire_selection'] : '';
+
+/* ===============================================================================================*
+*                            GESTION DES ERREURS et Validation
+ * ===========================================================================================
+ */
+
+if($carte_num == ''){
+    $err['carte_num'] = 'Numero de la carte bancaire obligatoire';
+}
+if($carte_num != '' && !preg_match("$[0-9]{16}$", $carte_num)) {
+    $err['carte_num'] = 'Numero de la carte bancaire doit contenir 16 chiffres.';
+}
+
+if($crypto == ''){
+    $err['crypto'] = 'cryptogramme de sécurité  obligatoire.';
+}
+
+if($crypto != '' && !preg_match("$[0-9]{3}$", $crypto)){
+    $err['crypto'] = 'cryptogramme de sécurité  doit contenir 3 chiffres.';
+}
+
+if($planning_horaire_selection == ''){
+    $err['planning_horaire_selection '] = 'Veuillez choisir l\'horaire de la consultation.';
+}
+
+if($client_id == '0'){
+    $err['client_id '] = 'Veuillez choisir l\'horaire de la consultation.';
+}
+
+$current_date = new DateTime();
+if($current_date->format('Y') > $expiration_annee  || ($current_date->format('Y') == $expiration_annee && $current_date->format('m') > $expiration_mois)){
+    $err['expiration '] = 'la date d\'expiration de la carte bancaire est invalide.';
+}
+
+
+/* ========================================================================== *
+ *                          ENREGISTREMENT KGESTION  COORDONNEE BANCAIRE                         *
+ * ========================================================================== */
+if(empty($err)){
+
+    $post_data = array(
+
+        'client_id'      => $client_id,
+        'planning_horaire_selection'      => $planning_horaire_selection,
+        'cartebancaires___name___prenom'      => $carte_prenom,
+        'cartebancaires___name___nom'      => $carte_nom,
+        'cartebancaires___name___numero'      => $carte_num,
+        'cartebancaires___name___expiration'      => $expiration_mois."/".substr($expiration_annee,2),
+        'cartebancaires___name___cryptogramme'      => $crypto
+    );
+
+        $kgestion_id = false;
+        $kgestion_insert = $kgestion->insertOnlineRDV($post_data);
+
+        if(!$kgestion_insert->success){
+            $err['sys'] = 'Système indisponible, veuillez réessayer plus tard.';
+        } else {
+            $kgestion_id = $kgestion_insert->id;
+            die(json_encode(array('rdv_added' => "Consultation ajoutée . Vous allez être appelé par l'un de nos conseiller pour avoir plus de détail")));
+
+        }
+
+
+} else {
+    die(json_encode(array('error'=>$err)));
+}
