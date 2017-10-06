e62186434fa298b2cedf571a79187545b43b36e2
sanadi <sanadi>
2017-10-06 18:51:00 +0100

    change design load for differentiate between page of tarot direct

--- include/visual_modules/tarot-direct/design-load.php
+++ include/visual_modules/tarot-direct/design-load.php
@@ -42,7 +42,9 @@ if($force_design && isset($styles[$_GET['css']])){
         $select_style = 'default';
     }
 }
-if(($_SERVER['REQUEST_URI'] == '/tarot-direct-rentree.php' || $_SERVER['REQUEST_URI'] == '/tarot-direct-rentree') || ($_SERVER['REQUEST_URI'] == '/tarot-direct-rentree-merci.php' || $_SERVER['REQUEST_URI'] == '/tarot-direct-rentree-merci')){
+//if(($_SERVER['REQUEST_URI'] == '/tarot-direct-rentree.php' || $_SERVER['REQUEST_URI'] == '/tarot-direct-rentree') || ($_SERVER['REQUEST_URI'] == '/tarot-direct-rentree-merci.php' || $_SERVER['REQUEST_URI'] == '/tarot-direct-rentree-merci')){
+if(strpos($_SERVER['REQUEST_URI'],'/tarot-direct-rentree') !== false){
+
     $select_style = 'rentree-17';
 
 }
