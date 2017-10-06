51695fde09f94310cd3abd27640d34d83bba6320
sanadi <sanadi>
2017-10-06 16:19:31 +0100

    fix carte position on every page

--- tarot-direct-rentree-dri-tog.php
+++ tarot-direct-rentree-dri-tog.php
@@ -54,7 +54,7 @@ if(isset($_SESSION['user_id'])){
             </div>
         </header>
 <!-- ------------------------------ FORMULAIRE ----------------------------- -->
-        <section class="ContentBand Form Principal" id="scn-form">
+        <section class="ContentBand Form Principal FormDRI" id="scn-form">
             <header class="ContentBand-ColumnHeader Title">- Pour poursuivre remplissez le <strong>formulaire</strong> ci-dessous -</header>
             <div class="PageWrapper ContentBand-Table">
                 <div class="ContentBand-Column">
