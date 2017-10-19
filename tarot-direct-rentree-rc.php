f5cf700dc9c07bf112ea13a5e79097468344eb89
sanadi <sanadi>
2017-10-19 16:49:00 +0100

    remove checkbox on the form and fix bug on the thanks page

--- tarot-direct-rentree-rc.php
+++ tarot-direct-rentree-rc.php
@@ -219,12 +219,6 @@ $_SESSION['reflexcash_source'] = isset($_GET['t3']) ? $_GET['t3'] : '';
                                  </div>
                                 </div>
 
-                                <div class="FormField radio">
-                                    <input type="radio" name="cguv"  id="cguv"  >
-                                    <label for="cguv" class="cguv">J'ai lu et j'accepte les conditions générales</label><br>
-                                    <input type="radio" name="partenaires" id="partenaires" >
-                                    <label for="partenaires" class="cguv">J'accepte de recevoir les offres des partenaires(conditions et desinscription)</label>
-                                </div>
 
                                 <button class="FormContainer-Submit" type="submit" name="valider">Lancer lʼinterprétation</button>
                         </article>
