190a375a9cb884be8bcebde9a2830111015373e2
mendez <nicolas.mendez@hotmail.fr>
2017-12-01 15:44:44 +0100

    gpbl

--- include/pixels/gpbl.php
+++ include/pixels/gpbl.php
@@ -1,5 +1,5 @@
 <?php
-$gpbl = $_SESSION['affiliation'];
+$gpbl = $_SESSION['affiliation'] ? : "";
 if($gpbl == "gpbl"){
 	echo '<img src="https://gpbl.go2cloud.org/aff_l?offer_id=98" width="1" height="1"/>';
 }
