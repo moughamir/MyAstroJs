f34ade92a2c8d4d8394390b13f72d4d3c8eafae0
MOUGHAMIR Mohamed <moughamir@gmail.com>
2017-12-12 17:01:30 +0000

    Scroll up * add scroll bg image * add bounce animation

--- gv18.php
+++ gv18.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html>
+
   <head>
     <meta charset="utf-8">
     <title>GV-18</title>
-    <link rel="stylesheet" href="css/grande-voyance.css" type="text/css" />
-  </head>
+    <link rel="stylesheet" href="css/grande-voyance.css" type="text/css" /> </head>
+
   <body class="no-js" id="gvApp">
-    <header></header>
     <section class="fullview">
+      <header class="page-header">
+        <h1 class="title">La grande voyance <?php echo date('Y')+1 ?>
+        </h1>
+      </header>
       <div class="scroll"></div>
     </section>
     <section class="fullview">
@@ -28,9 +32,9 @@
     <section class="fullview">
       <div class="scroll"></div>
     </section>
-    <section class="fullview">
-    </section>
+    <section class="fullview"> </section>
     <script type="text/javascript" src="//cdn.jsdelivr.net/gh/jquery/jquery@3.2.1/dist/jquery.min.js"></script>
     <script type="text/javascript" src="js/grande-voyance.js"></script>
   </body>
+
 </html>
