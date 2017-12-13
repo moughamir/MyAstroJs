edd5836ea0aa6c5390dae4c77335eb449e992413
MOUGHAMIR Mohamed <moughamir@gmail.com>
2017-12-13 10:01:22 +0000

    Intro the grid

--- gv18.php
+++ gv18.php
@@ -2,16 +2,29 @@
 <html>
 
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <title>GV-18</title>
-    <link rel="stylesheet" href="css/grande-voyance.css" type="text/css" /> </head>
+    <link rel="stylesheet" href="css/grande-voyance.css" type="text/css" />
+    <link href="//fonts.googleapis.com/css?family=Open+Sans|Oswald" rel="stylesheet">
+  </head>
 
   <body class="no-js" id="gvApp">
     <section class="fullview">
       <header class="page-header">
-        <h1 class="title">La grande voyance <?php echo date('Y')+1 ?>
-        </h1>
+        <h1 class="title">La grande voyance <?php echo date('Y')+1 ?></h1>
+        <h2 class="headline">nouvelle année, nouveau départ, nouvelle vie</h2>
       </header>
+      <div class="article-container">
+        <article class="page-article" id="intro">
+          <h3 class="article-headline">Intro</h3>
+          <p class="article-paragraph">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+          sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+          magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+          quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+          ut aliquip ex ea commodo consequat.</p>
+          <span class="article-readmore">en savoir plus</span>
+        </article>
+      </div>
       <div class="scroll"></div>
     </section>
     <section class="fullview">
