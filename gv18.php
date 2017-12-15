4d16e308650fff47231a81050c3558afb7fe6f1c
MOUGHAMIR Mohamed <moughamir@gmail.com>
2017-12-15 09:33:34 +0000

    first portion vvc

--- gv18.php
+++ gv18.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <title>GV-18</title>
     <link rel="stylesheet" href="css/grande-voyance.css" type="text/css" />
-    <link href="//fonts.googleapis.com/css?family=Open+Sans|Oswald" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Lobster|Open+Sans|Oswald" rel="stylesheet"> 
   </head>
 
   <body class="no-js" id="gvApp">
@@ -16,36 +16,59 @@
       </header>
       <div class="article-container">
         <article class="page-article" id="intro">
+          <aside class="article-aside">
+            <h3>Taurus</h3>
+          </aside>
           <h3 class="article-headline">Intro</h3>
           <p class="article-paragraph">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
           sed diam nonummy nibh euismod tincidunt ut laoreet dolore
           magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
           quis nostrud exerci tation ullamcorper suscipit lobortis nisl
           ut aliquip ex ea commodo consequat.</p>
-          <span class="article-readmore">en savoir plus</span>
+         
         </article>
+        <div class="article-more-container">
+          <div class="article-more">En savoir plus</div>
+        </div>
       </div>
-      <div class="scroll"></div>
+      <div class="scroll-container">
+        <div class="scroll"></div>
+      </div>
+      
     </section>
     <section class="fullview">
-      <div class="scroll"></div>
+      <div class="scroll-container">
+        <div class="scroll"></div>
+      </div>
     </section>
     <section class="fullview">
-      <div class="scroll"></div>
+      <div class="scroll-container">
+        <div class="scroll"></div>
+      </div>
     </section>
     <section class="fullview">
-      <div class="scroll"></div>
+      <div class="scroll-container">
+        <div class="scroll"></div>
+      </div>
     </section>
     <section class="fullview">
-      <div class="scroll"></div>
+      <div class="scroll-container">
+        <div class="scroll"></div>
+      </div>
     </section>
     <section class="fullview">
-      <div class="scroll"></div>
+      <div class="scroll-container">
+        <div class="scroll"></div>
+      </div>
     </section>
     <section class="fullview">
-      <div class="scroll"></div>
+      <div class="scroll-container">
+        <div class="scroll"></div>
+      </div>
     </section>
-    <section class="fullview"> </section>
+    <section class="fullview">
+      
+    </section>
     <script type="text/javascript" src="//cdn.jsdelivr.net/gh/jquery/jquery@3.2.1/dist/jquery.min.js"></script>
     <script type="text/javascript" src="js/grande-voyance.js"></script>
   </body>
