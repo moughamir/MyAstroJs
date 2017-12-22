1b7259f4bc1ed855d65b5b5b055c7e38de8f7fe2
MOUGHAMIR Mohamed <moughamir@gmail.com>
2017-12-22 13:26:47 +0000

    Switch from grid layout to flex due to iE bug

--- gv18.php
+++ gv18.php
@@ -5,79 +5,259 @@
     <meta charset="utf-8" />
     <title>GV-18</title>
     <link rel="stylesheet" href="css/grande-voyance.css" type="text/css" />
-    <link href="//fonts.googleapis.com/css?family=Lobster|Open+Sans|Oswald" rel="stylesheet"> 
-  </head>
+    <link href="//fonts.googleapis.com/css?family=Lobster|Open+Sans|Oswald" rel="stylesheet" /> </head>
 
   <body class="no-js" id="gvApp">
-    <section class="fullview">
+    <div class="loading"></div>
+    <section class="section section-intro">
       <header class="page-header">
-        <h1 class="title">La grande voyance <?php echo date('Y')+1 ?></h1>
-        <h2 class="headline">nouvelle année, nouveau départ, nouvelle vie</h2>
-      </header>
-      <div class="article-container">
-        <article class="page-article" id="intro">
-          <aside class="article-aside">
-            <h3 id="title">Taurus</h3>
-          </aside>
-          <h3 class="article-headline">Intro</h3>
-          <p class="article-paragraph">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-          sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-          magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
-          quis nostrud exerci tation ullamcorper suscipit lobortis nisl
-          ut aliquip ex ea commodo consequat.</p>
-         
-        </article>
-        <div class="article-more-container">
-          <div class="article-more">En savoir plus</div>
+        <div class="title-container">
+          <h1 class="title">La grande voyance 2018</h1>
+          <h2 class="headline">nouvelle année, nouveau départ, nouvelle vie</h2>
         </div>
-      </div>
+      </header>
+      <main class="article-wrapper">
+        <div class="article-container">
+          <div class="article">
+            <article class="page-article" id="intro">
+              <aside class="article-aside">
+                <h3 id="title">{{zodiacName}}</h3>
+              </aside>
+              <h3 class="article-headline">Intro</h3>
+              <p class="article-paragraph">{{intro}}</p>
+            </article>
+            <div class="article-more-container">
+              <div class="article-more">En savoir plus</div>
+            </div>
+          </div>
+        </div>
+      </main>
       <div class="scroll-container">
         <div class="scroll"></div>
       </div>
     </section>
-    <section class="fullview">
-      <div class="article-container">
-        <article class="page-article" id="love">
-          <aside class="article-aside"><h3>Amour</h3></aside>
-          <h3 class="article-headline">En couple</h3>
-          <p class="article-paragraph">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-          sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-          magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
-          quis nostrud exerci tation ullamcorper suscipit lobortis nisl
-          ut aliquip ex ea commodo consequat.</p>
-        </article>
-      </div>
+    <section class="section section-love">
+      <header class="page-header"> </header>
+      <main class="article-wrapper">
+        <div class="article-container">
+          <div class="article" id="love">
+            <article class="page-article love-c">
+              <aside class="article-aside">
+                <h3 id="title"><span class="huge">A</span>mour</h3>
+              </aside>
+              <h3 class="article-headline">En Couple</h3>
+              <p class="article-paragraph" id="couple">{{coupleParagraph}}</p>
+            </article>
+            <article class="page-article love-s">
+              <h3 class="article-headline">Célib</h3>
+              <p class="article-paragraph" id="single">{{singleParagraph}}</p>
+              <div class="article-more">En savoir plus</div>
+            </article>
+          </div>
+        </div>
+      </main>
       <div class="scroll-container">
         <div class="scroll"></div>
       </div>
     </section>
-    <section class="fullview">
+    <section class="section section-social">
+      <header class="page-header"> </header>
+      <main class="article-wrapper">
+        <div class="article-container">
+          <div class="article-two" id="social">
+            <h3 id="title"><span class="huge">V</span>ie social</h3>
+            <div class="social-container">
+              <article class="page-article">
+                <h3 class="article-headline">Famille</h3>
+                <p class="article-paragraph" id="family">{{socialFam}}</p>
+              </article>
+              <article class="page-article">
+                <h3 class="article-headline">Amitié</h3>
+                <p class="article-paragraph" id="friendship">{{socialFri}}</p>
+                <div class="article-more">En savoir plus</div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </main>
       <div class="scroll-container">
         <div class="scroll"></div>
       </div>
     </section>
-    <section class="fullview">
+    <section class="section section-pro">
+      <header class="page-header"> </header>
+      <main class="article-wrapper">
+        <div class="article-container">
+          <div class="article pro">
+            <aside class="pro-aside">
+              <h3 id="title"><span class="huge">V</span>ie active</h3>
+            </aside>
+            <article class="page-article">
+              <h3 class="article-headline">Travail</h3>
+              <p class="article-paragraph" id="work">{{workParagraph}}</p>
+              <div class="article-more">Je veux en savoir plus</div>
+            </article>
+          </div>
+        </div>
+      </main>
       <div class="scroll-container">
         <div class="scroll"></div>
       </div>
     </section>
-    <section class="fullview">
+    <section class="section section-cal">
+      <header class="page-header">
+        <div class="article-header"><span class="huge">2018</span> mois par mois</div>
+      </header>
+      <main class="article-wrapper cal">
+        <div class="article-container"> <a href="#moCarousel" class="mo-control left" id="prev"><span class="nav nav-prev"></span></a>
+          <div class="mo-wrapper">
+            <ul class="mo-container">
+              <li class="mo-block">
+                <div class="mo-article" id="1">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="2">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="3">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="4">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="5">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="6">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="7">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="8">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="9">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="10">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="11">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+              <li class="mo-block">
+                <div class="mo-article" id="12">
+                  <h3>{{month}}</h3>
+                  <p>{{monthParagraph}}</p>
+                </div>
+              </li>
+            </ul>
+          </div> <a href="#moCarousel" class="mo-control right" id="next"><span class="nav nav-next"></span></a> </div>
+      </main>
       <div class="scroll-container">
         <div class="scroll"></div>
       </div>
     </section>
-    <section class="fullview">
+    <section class="section section-numero">
+      <header class="page-header"> </header>
+      <main class="article-wrapper">
+        <div class="article-container">
+          <div class="article number-art">
+            <h3 id="title"><span class="huge">V</span>otre chiffre chance</h3>
+            <article class="page-article" id="sifre">
+              <aside class="article-aside"> <span class="number">{{luckyNumber}}</span> </aside>
+              <p class="article-paragraph">luckyNumberText</p>
+              <div class="article-more">En savoir plus</div>
+            </article>
+          </div>
+        </div>
+      </main>
       <div class="scroll-container">
         <div class="scroll"></div>
       </div>
     </section>
-    <section class="fullview">
+    <section class="section section-dates">
+      <header class="page-header">
+       
+      </header>
+      <main class="article-wrapper">
+        <div class="article-container">
+          <div class="article" id="dates">
+            <div class="section-dates-wrapper">
+               <h3><span class="huge">V</span>os dates clés</h3>
+              <article class="page-article">
+                <h4 class="article-date">{{when}}</h4>
+                <p class="article-paragraph">{{what}}</p>
+              </article>
+            </div>
+            <div class="section-dates-wrapper">
+              <article class="page-article">
+                <h4 class="article-date">{{when}}</h4>
+                <p class="article-paragraph">{{what}}</p>
+              </article>
+            </div>
+            <div class="section-dates-wrapper">
+              <article class="page-article">
+                <h4 class="article-date">{{when}}</h4>
+                <p class="article-paragraph">{{what}}</p>
+              </article>
+            </div>
+          </div>
+        </div>
+      </main>
       <div class="scroll-container">
         <div class="scroll"></div>
       </div>
     </section>
-    <section class="fullview">
-      
+    <section class="section section-addon">
+      <header class="page-header"> </header>
+      <main class="article-wrapper">
+        <div class="article-container addon">
+          <header class="article-header headline headline-adv">Nos conseils</header>
+          <article class="section-addon-content" id="conseils">
+            <p>{{advices}}</p>
+          </article>
+        </div>
+        <div class="article-container addon">
+          <header class="article-header headline headline-obj">Vos objectifs</header>
+          <article class="section-addon-content" id="objectifs">
+            <p>{{objectives}}</p>
+          </article>
+        </div>
+      </main>
     </section>
     <script type="text/javascript" src="//cdn.jsdelivr.net/gh/jquery/jquery@3.2.1/dist/jquery.min.js"></script>
     <script type="text/javascript" src="js/grande-voyance.js"></script>
