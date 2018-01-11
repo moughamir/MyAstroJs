4fcd70272eb0669b1dd80700558303479f2de6b2
mendez <nicolas.mendez@hotmail.fr>
2018-01-11 07:54:22 +0100

    tchat

--- tarot-tchat-dri.php
+++ tarot-tchat-dri.php
@@ -79,7 +79,7 @@ if(isset($_SESSION['user_id'])){
                                 <div class="offer-tel"></div>
                             </div>
                             <div class="offer-c">
-                                <a href="https://voyance-en-direct.tv/tarot-tchat/offre-gratuite?id=<?= $kgestion_id ?>" class="btn">Commencer le Tchat Gratuit</a>
+                                <a href="https://voyance-en-direct.tv/tarot-tchat/saisie-cb?id=<?= $kgestion_id ?>" class="btn">Commencer le Tchat Gratuit</a>
                                 <div class="offer-tchat"></div>
                             </div>
                         </div>
