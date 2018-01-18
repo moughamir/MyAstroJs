8fc8c981b5261d35635debf5cb7097e804b0eb81
mendez <nicolas.mendez@hotmail.fr>
2018-01-18 16:56:56 +0100

    gv18

--- include/gv18-tab.php
+++ include/gv18-tab.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
 $prefix = (!empty($prefix) ? $prefix : 'gv18');
-$parms = explode('&', $_SERVER['QUERY_STRING']);
+$parms = !empty($_SERVER['QUERY_STRING']) ? explode('&', $_SERVER['QUERY_STRING']) : [];
 if(isset($_GET['t'])) {
     $curUrl = strtok($_SERVER["REQUEST_URI"],'?');
     $_SESSION['gv'] = $_GET['t'];
     $p= $_GET['p'];
     $indexSpam = array_search('t='.$_GET['t'], $parms);
     unset($parms[$indexSpam]);
-    header("location: ".$curUrl."?".join('&', $parms));
+    $newParams = count($parms) ? "?".join('&', $parms) : '';
+    header("location: ".$curUrl.$newParams);
 }
 
 $correspondance = [
