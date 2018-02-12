2cf67cf6a6f94d67ab374f6878d9220edd8da94b
MOUGHAMIR Mohamed <moughamir@gmail.com>
2018-02-12 18:44:39 +0000

    Updated the arch for better dev and prod performance

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+//import AwesomeComponent from './components/AwesomeComponent.jsx';
+//import { Button, Container, Divider, Grid, Header, Icon, Image, List, Menu, Responsive, Segment, Sidebar, Visibility, } from 'semantic-ui-react'
+
+/* eslint-disable react/no-multi-comp */
+import Home from './Pages/HomePage';
+
+const App = () => (
+  <Home />
+);
+
+export default App;
---
+++ src/Components/AwesomeComponent.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+class AwesomeComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { likesCount: 0 };
+    this.onLike = this.onLike.bind(this);
+  }
+
+  onLike() {
+    let newLikesCount = this.state.likesCount + 1;
+    this.setState({ likesCount: newLikesCount });
+  }
+
+  render() {
+    return (
+      <div>
+        Like : <span>{this.state.likesCount}</span>
+        <div>
+          <Button onClick={this.onLike}>Like Me</Button>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default AwesomeComponent;
