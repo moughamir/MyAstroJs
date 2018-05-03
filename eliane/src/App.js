f47612fb113eef8690ec63b2abd9822187f9c074
MOUGHAMIR Mohamed <moughamir@gmail.com>
2018-05-03 17:35:24 +0000

    Icons

--- eliane/src/App.js
+++ eliane/src/App.js
@@ -14,9 +14,29 @@ class App extends React.Component {
         speciality: 'Tarologue',
         image: psychImage
       },
+      intro: 'Bonjour,<br/>Je suis Éliane voyante intuitive, mais le mot médium doit davantage vous parler. Cartomancienne, j’ai régulièrement recours au tarot de Marseille pour m’apporter un supportphysique et approfondir mes visions et mes ressentis.<br/> Voilà maintenant plus de trente années que me sont apparus mes dons de médiumnité pour la première fois.<br/> Une aventure qui m’a fait redécouvrir l’Humain, une notion que je place au centre de toutes mes consultations.<br/> Parce que sans vous mes visions n’ont plus aucune signification, vous êtes la clé de toute ma passion.',
+      domaine: [{
+          name: 'L\'amour',
+          icon: 'ico-love'
+        },{
+          name: 'Le travail',
+          icon: 'ico-work'
+        },
+        {
+          name: 'L\'argent',
+          icon: 'ico-money'
+        }
+]
     };
   }
+
+
+
   render() {
+
+    const Domaines = this.state.domaine;
+    console.log(Domaines)
+
     return (
       <div className="App">
         <header className="App-header">
@@ -35,7 +55,22 @@ class App extends React.Component {
             </div>
           </aside>
         </section>
-        <section className="col-2 col-right" style={style.rightSide}></section>
+        <section className="col-2 col-right" style={style.rightSide}>
+          <article className='bloc-form'>
+            <p dangerouslySetInnerHTML={{__html: this.state.intro}}></p>
+            <p>Pour commencer</p>
+            <h3>Choissiez votre domaine :</h3>
+            <div>
+              <ul>
+              {
+                Domaines.map((item, index) => {
+                  return <li key={index}><i className={"ico "+item.icon}></i> {item.name}</li>;
+                })
+              }
+              </ul>
+            </div>
+          </article>
+        </section>
       </div>
     );
   }
