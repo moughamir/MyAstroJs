8160fd91f3a61a07fcd376eb8314e54858fe6255
MOUGHAMIR Mohamed <moughamir@gmail.com>
2018-05-04 13:56:38 +0000

    Add Button and Icons

--- src/eliane/src/App.js
+++ src/eliane/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import './assets/icons/flaticon.css';
 import style from './variables';
 import psychImage from './assets/psych.png';
 
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       psych: {
@@ -17,25 +17,34 @@ class App extends React.Component {
       intro: 'Bonjour,<br/>Je suis Éliane voyante intuitive, mais le mot médium doit davantage vous parler. Cartomancienne, j’ai régulièrement recours au tarot de Marseille pour m’apporter un supportphysique et approfondir mes visions et mes ressentis.<br/> Voilà maintenant plus de trente années que me sont apparus mes dons de médiumnité pour la première fois.<br/> Une aventure qui m’a fait redécouvrir l’Humain, une notion que je place au centre de toutes mes consultations.<br/> Parce que sans vous mes visions n’ont plus aucune signification, vous êtes la clé de toute ma passion.',
       domaine: [{
           name: 'L\'amour',
-          icon: 'ico-love'
-        },{
+          ref: 'love',
+          icon: 'flaticon-heart'
+        }, {
           name: 'Le travail',
-          icon: 'ico-work'
+          ref: 'work',
+          icon: 'flaticon-case'
         },
         {
           name: 'L\'argent',
-          icon: 'ico-money'
-        }
-]
+          ref: 'money',
+          icon: 'flaticon-bag'
+        }],
+      target: ''
     };
+    this.setDomain = this.setDomain.bind(this);
+  }
+
+  componentDidUpdate() {
+    console.log('Component Did Upadate : ' + this.state.target);
+  }
+
+  setDomain(ref){
+    this.setState({target: ref.target.dataset.ref});
   }
 
 
-
   render() {
-
     const Domaines = this.state.domaine;
-    console.log(Domaines)
 
     return (
       <div className="App">
@@ -64,16 +73,21 @@ class App extends React.Component {
               <ul>
               {
                 Domaines.map((item, index) => {
-                  return <li key={index}><i className={"ico "+item.icon}></i> {item.name}</li>;
+                  return (
+                  <li key={index}>
+                    <button className="btn-select" data-ref={item.ref} onClick={this.setDomain}>
+                      <i className={'ico ' + item.icon} ></i>
+                      <span className='item-name'>{item.name}</span>
+                    </button>
+                  </li>
+                  );
                 })
               }
               </ul>
             </div>
           </article>
         </section>
-      </div>
-    );
+      </div>);
   }
 }
-
 export default App;
