1a8dab6fe558c059c53ef86250a9996c3530e45f
MOUGHAMIR Mohamed <moughamir@gmail.com>
2018-05-14 17:38:29 +0000

    Removed initial ReactJs code and replaced it with Native code with jQuery

--- eliane.php
+++ eliane.php
@@ -7,6 +7,9 @@
     <link rel="shortcut icon" href="favicon.ico">
     <title>Éliane</title>
     <link rel="stylesheet" href="css/eliane.css" type="text/css" />
+    <!--[if lt IE 9]>
+    <script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3,respond@1.4.2"></script>
+    <![endif]-->
   </head>
 
   <body>
@@ -28,7 +31,7 @@
         </aside>
       </section>
       <section class="col-2 col-right">
-        <article class='bloc-form' id="intro">
+        <article class='bloc-form hidden' id="intro">
           <p>Bonjour,<br/>Je suis Éliane voyante intuitive, mais le mot médium doit davantage vous parler. Cartomancienne, j’ai régulièrement recours au tarot de Marseille pour m’apporter un supportphysique et approfondir mes visions et mes ressentis.<br/> Voilà maintenant plus de trente années que me sont apparus mes dons de médiumnité pour la première fois.<br/> Une aventure qui m’a fait redécouvrir l’Humain, une notion que je place au centre de toutes mes consultations.<br/> Parce que sans vous mes visions n’ont plus aucune signification, vous êtes la clé de toute ma passion.</p>
           <p>Pour commencer</p>
           <h3>Choissiez votre domaine :</h3>
@@ -55,12 +58,67 @@
             </ul>
           </div>
         </article>
-        <article class='bloc-form' id="theme"></article>
-        <article class='bloc-form' id="form"></article>
-        <article class='bloc-form' id="dri"></article>
+        <article class='bloc-form' id="theme">
+          <header class="bloc-form--header">
+            <div class="ico-container">
+              <i class="ico flaticon-heart"></i>
+              <span class='item-name'>L'amour</span>
+            </div>
+          </header>
+            <div class="form-container hidden">
+              <div class="form-input--group">
+                <label for="theme">Thème de votre question</label>
+                <select name="theme" class="input" id="input-theme">
+                  <option value="love">amour</option>
+                  <option value="work">travail</option>
+                  <option value="money">argent</option>
+                </select>
+              </div>
+              <div class="form-input--group">
+                <label for="conjoint">Prénom de l'être aimé</label>
+                <input type="text" class="input" name="conjoint" placeholder="Prénom de l'être aimé"/>
+              </div>
+              <div class="form-input--text">
+                <label for="demande">Précisez-moi votre demande :</label>
+                <textarea class="input-textarea" class="input" name="message" placeholder="Précisez-moi votre demande..."></textarea>
+              </div>
+              <div class="form-input--btn">
+                <button class="btn-send">Envoyer</button>
+              </div>
+            </div>
+            <div class="form-container">
+              <div class="form-headline">
+                <p>Remplissez vite le formulaire afin que <br/>
+                je puisse vous envoyer votre interprétation :</p>
+              </div>
+              <div class="form-input--group">
+                <label for="theme">Thème de votre question</label>
+                <select name="theme" class="input" id="input-theme">
+                  <option value="love">amour</option>
+                  <option value="work">travail</option>
+                  <option value="money">argent</option>
+                </select>
+              </div>
+              <div class="form-input--group">
+                <label for="conjoint">Prénom de l'être aimé</label>
+                <input type="text" class="input" name="conjoint" placeholder="Prénom de l'être aimé"/>
+              </div>
+              <div class="form-input--text">
+                <label for="demande">Précisez-moi votre demande :</label>
+                <textarea class="input-textarea" class="input" name="message" placeholder="Précisez-moi votre demande..."></textarea>
+              </div>
+              <div class="form-input--btn">
+                <button class="btn-send">Envoyer</button>
+              </div>
+            </div>
+          </article>
+        <article class='bloc-form hidden' id="dri"></article>
       </section>
     </div>
-
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/g/lodash@4.17.4"></script>
+    <script type="text/javascript" src="js/formValidator.js"></script>
     <script type="text/javascript" src="js/eliane.js"></script>
   </body>
 
--- js/eliane.js
+++ js/eliane.js
@@ -1,9 +1,7 @@
-const psychImage = '';
 const state = {
   psych: {
     name: 'Éliane Retois',
-    speciality: 'Tarologue',
-    image: psychImage
+    speciality: 'Tarologue'
   },
   intro: 'Bonjour,<br/>Je suis Éliane voyante intuitive, mais le mot médium doit davantage vous parler. Cartomancienne, j’ai régulièrement recours au tarot de Marseille pour m’apporter un supportphysique et approfondir mes visions et mes ressentis.<br/> Voilà maintenant plus de trente années que me sont apparus mes dons de médiumnité pour la première fois.<br/> Une aventure qui m’a fait redécouvrir l’Humain, une notion que je place au centre de toutes mes consultations.<br/> Parce que sans vous mes visions n’ont plus aucune signification, vous êtes la clé de toute ma passion.',
   domaine: [{
@@ -21,18 +19,19 @@ const state = {
       icon: 'flaticon-bag'
         }],
 };
-var target = '';
 
-console.log('H')
-
-const $item =  document.querySelectorAll('.item-select');
+var target;
 
 document.addEventListener('DOMContentLoaded', function() {
+
+  const $item = document.querySelectorAll('.item-select');
+
   // click button to set target domain and display next step
   [].forEach.call($item, function(el) {
-  el.addEventListener('click', function() {
-    target = el.children[0].dataset.ref
-    console.log(target)
-  })
-})
-})
+    el.addEventListener('click', function() {
+      console.log(this)
+      target = this.children[0].dataset.ref;
+      console.log(target)
+    });
+  });
+});
