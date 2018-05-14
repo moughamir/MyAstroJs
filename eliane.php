8bcb9188185429e277d066c99a93eaaa8b55f8cc
MOUGHAMIR Mohamed <moughamir@gmail.com>
2018-05-14 17:59:01 +0000

    Add form

--- eliane.php
+++ eliane.php
@@ -24,7 +24,7 @@
       </header>
       <section class="col-2 col-left">
         <aside class="bloc-psy">
-          <img src="images_landing/eliane/psych.png" alt="Éliane Retois Tarologue"/>
+          <img src="images_landing/eliane/psych.png" alt="Éliane Retois Tarologue" />
           <div class="bloc-psy--info">
             <p>Éliane Retois,<br/> Tarologue</p>
           </div>
@@ -32,7 +32,8 @@
       </section>
       <section class="col-2 col-right">
         <article class='bloc-form hidden' id="intro">
-          <p>Bonjour,<br/>Je suis Éliane voyante intuitive, mais le mot médium doit davantage vous parler. Cartomancienne, j’ai régulièrement recours au tarot de Marseille pour m’apporter un supportphysique et approfondir mes visions et mes ressentis.<br/> Voilà maintenant plus de trente années que me sont apparus mes dons de médiumnité pour la première fois.<br/> Une aventure qui m’a fait redécouvrir l’Humain, une notion que je place au centre de toutes mes consultations.<br/> Parce que sans vous mes visions n’ont plus aucune signification, vous êtes la clé de toute ma passion.</p>
+          <p>Bonjour,<br/>Je suis Éliane voyante intuitive, mais le mot médium doit davantage vous parler. Cartomancienne, j’ai régulièrement recours au tarot de Marseille pour m’apporter un supportphysique et approfondir mes visions et mes ressentis.<br/>            Voilà maintenant plus de trente années que me sont apparus mes dons de médiumnité pour la première fois.<br/> Une aventure qui m’a fait redécouvrir l’Humain, une notion que je place au centre de toutes mes consultations.<br/> Parce que sans
+            vous mes visions n’ont plus aucune signification, vous êtes la clé de toute ma passion.</p>
           <p>Pour commencer</p>
           <h3>Choissiez votre domaine :</h3>
           <div>
@@ -65,53 +66,85 @@
               <span class='item-name'>L'amour</span>
             </div>
           </header>
-            <div class="form-container hidden">
-              <div class="form-input--group">
-                <label for="theme">Thème de votre question</label>
-                <select name="theme" class="input" id="input-theme">
+          <div class="form-container hidden">
+            <div class="form-input--group">
+              <label for="theme">Thème de votre question</label>
+              <select name="theme" class="input" id="input-theme">
                   <option value="love">amour</option>
                   <option value="work">travail</option>
                   <option value="money">argent</option>
                 </select>
-              </div>
-              <div class="form-input--group">
-                <label for="conjoint">Prénom de l'être aimé</label>
-                <input type="text" class="input" name="conjoint" placeholder="Prénom de l'être aimé"/>
-              </div>
-              <div class="form-input--text">
-                <label for="demande">Précisez-moi votre demande :</label>
-                <textarea class="input-textarea" class="input" name="message" placeholder="Précisez-moi votre demande..."></textarea>
-              </div>
-              <div class="form-input--btn">
-                <button class="btn-send">Envoyer</button>
-              </div>
             </div>
-            <div class="form-container">
-              <div class="form-headline">
-                <p>Remplissez vite le formulaire afin que <br/>
-                je puisse vous envoyer votre interprétation :</p>
-              </div>
-              <div class="form-input--group">
-                <label for="theme">Thème de votre question</label>
-                <select name="theme" class="input" id="input-theme">
+            <div class="form-input--group">
+              <label for="conjoint">Prénom de l'être aimé</label>
+              <input type="text" class="input" name="conjoint" placeholder="Prénom de l'être aimé" />
+            </div>
+            <div class="form-input--text">
+              <label for="demande">Précisez-moi votre demande :</label>
+              <textarea class="input-textarea" class="input" name="message" placeholder="Précisez-moi votre demande..."></textarea>
+            </div>
+            <div class="form-input--btn">
+              <button class="btn-send">Envoyer</button>
+            </div>
+          </div>
+          <div class="form-container">
+            <div class="form-headline">
+              <p>Remplissez vite le formulaire afin que <br/> je puisse vous envoyer votre interprétation :</p>
+            </div>
+            <div class="form">
+              <form class="ajax">
+                <input type="hidden" name="site" value="MyAstro" />
+                <input type="hidden" name="source" value="<?= $source;?>" />
+                <input type="hidden" name="method" value="<?= $method;?>" />
+                <input type="hidden" name="support" value="<?= $support;?>" />
+                <input type="hidden" name="affiliation" value="<?= $affiliation;?>" />
+                <input type="hidden" name="dri" value="<?= $dri;?>" />
+                <input type="hidden" name="redirect_method" value="reload_form" />
+                <input type="hidden" name="convertir" value="1" />
+                <input type="hidden" name="tel_needed" value="0" />
+                <input type="hidden" name="cguv" value="1" />
+                <input type="hidden" name="partenaires" value="1" />
+                <input type="hidden" name="gclid" value="<?= $gclid;?>" />
+                <input type="hidden" name="pays" value="FR" />
+                <input type="hidden" name="question_code" value="<?= $question;?>" />
+
+                <div class="FormField radio">
+                  <div class="FormField-TableInputContainer fixed-2-col gender">
+                    <div class="FormField-TableInputContainer-Cell">
+                      <label for="sexe-f" class="FormField-Label ">
+                        <span class="ico-woman"></span>
+                        une femme</label>
+                      <input type="radio" name="sexe" value="femme" id="sexe-f" class="FormField-Input" /> </div>
+                    <div class="FormField-TableInputContainer-Cell">
+                      <label for="sexe-h" class="FormField-Label">
+                        <span class="ico-man"></span>
+                        un homme</label>
+                      <input type="radio" name="sexe" value="homme" id="sexe-h" class="FormField-Input" /> </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="form-input--group">
+              <label for="theme">Thème de votre question</label>
+              <select name="theme" class="input" id="input-theme">
                   <option value="love">amour</option>
                   <option value="work">travail</option>
                   <option value="money">argent</option>
                 </select>
-              </div>
-              <div class="form-input--group">
-                <label for="conjoint">Prénom de l'être aimé</label>
-                <input type="text" class="input" name="conjoint" placeholder="Prénom de l'être aimé"/>
-              </div>
-              <div class="form-input--text">
-                <label for="demande">Précisez-moi votre demande :</label>
-                <textarea class="input-textarea" class="input" name="message" placeholder="Précisez-moi votre demande..."></textarea>
-              </div>
-              <div class="form-input--btn">
-                <button class="btn-send">Envoyer</button>
-              </div>
             </div>
-          </article>
+            <div class="form-input--group">
+              <label for="conjoint">Prénom de l'être aimé</label>
+              <input type="text" class="input" name="conjoint" placeholder="Prénom de l'être aimé" />
+            </div>
+            <div class="form-input--text">
+              <label for="demande">Précisez-moi votre demande :</label>
+              <textarea class="input-textarea" class="input" name="message" placeholder="Précisez-moi votre demande..."></textarea>
+            </div>
+            <div class="form-input--btn">
+              <button class="btn-send">Envoyer</button>
+            </div>
+          </div>
+        </article>
         <article class='bloc-form hidden' id="dri"></article>
       </section>
     </div>
